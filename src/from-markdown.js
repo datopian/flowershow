c737d08c74576ad089180c9a87bdb0d065cea87e
khalilcodes <khalilali.j@gmail.com>
2022-03-31 23:46:41 +0300

    custom helper files

---
+++ src/from-markdown.js
@@ -0,0 +1,96 @@
+function fromMarkdown (opts = {}) {
+  const permalinks = opts.permalinks || []
+  const defaultPageResolver = (name) => [name.replace(/ /g, '-').toLowerCase()]
+  const pageResolver = opts.pageResolver || defaultPageResolver
+  const newClassName = opts.newClassName || 'new'
+  const wikiLinkClassName = opts.wikiLinkClassName || 'internal'
+  const defaultHrefTemplate = (permalink) => `/${permalink}`
+  const hrefTemplate = opts.hrefTemplate || defaultHrefTemplate
+
+  function enterWikiLink (token) {
+    this.enter(
+      {
+        type: 'wikiLink',
+        value: null,
+        data: {
+          alias: null,
+          permalink: null,
+          exists: null
+        }
+      },
+      token
+    )
+  }
+
+  function top (stack) {
+    return stack[stack.length - 1]
+  }
+
+  function exitWikiLinkAlias (token) {
+    const alias = this.sliceSerialize(token)
+    const current = top(this.stack)
+    current.data.alias = alias
+  }
+
+  function exitWikiLinkTarget (token) {
+    const target = this.sliceSerialize(token)
+    const current = top(this.stack)
+    current.value = target
+  }
+
+  function exitWikiLink (token) {
+    const wikiLink = this.exit(token)
+
+    const pagePermalinks = pageResolver(wikiLink.value)
+    let permalink = pagePermalinks.find((p) => {
+      let heading = ''
+      if (p.match(/#/)) {
+        [, heading] = p.split('#')
+      }
+      const link = heading ? p.replace(`#${heading}`, '') : p
+      return permalinks.indexOf(link) !== -1
+    })
+    const exists = permalink !== undefined
+    if (!exists) {
+      permalink = pagePermalinks[0]
+    }
+    let displayName = wikiLink.value
+    if (wikiLink.data.alias) {
+      displayName = wikiLink.data.alias
+    }
+
+    let classNames = wikiLinkClassName
+    if (!exists) {
+      classNames += ' ' + newClassName
+    }
+
+    wikiLink.data.alias = displayName
+    wikiLink.data.permalink = permalink
+    wikiLink.data.exists = exists
+
+    wikiLink.data.hName = 'a'
+    wikiLink.data.hProperties = {
+      className: classNames,
+      href: hrefTemplate(permalink)
+    }
+    wikiLink.data.hChildren = [
+      {
+        type: 'text',
+        value: displayName
+      }
+    ]
+  }
+
+  return {
+    enter: {
+      wikiLink: enterWikiLink
+    },
+    exit: {
+      wikiLinkTarget: exitWikiLinkTarget,
+      wikiLinkAlias: exitWikiLinkAlias,
+      wikiLink: exitWikiLink
+    }
+  }
+}
+
+export { fromMarkdown }
