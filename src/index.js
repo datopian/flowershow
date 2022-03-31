a9acfadb8ba3d8c7113f0890890fa9f74c8c225b
khalilcodes <khalilali.j@gmail.com>
2022-03-31 23:50:41 +0300

    add options and resolve page urls

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { syntax } from 'micromark-extension-wiki-link'
-import { fromMarkdown, toMarkdown } from 'mdast-util-wiki-link'
+import { toMarkdown } from 'mdast-util-wiki-link'
+import { fromMarkdown } from './from-markdown'
+import { getFiles } from './getFiles'
 
 let warningIssued
 
-function wikiLinkPlugin (opts = {}) {
+function wikiLinkPlugin (opts = { markdownFolder: '' }) {
   const data = this.data()
 
   function add (field, value) {
@@ -22,6 +24,27 @@ function wikiLinkPlugin (opts = {}) {
     console.warn(
       '[remark-wiki-link] Warning: please upgrade to remark 13 to use this plugin'
     )
+  }
+
+  opts = {
+    ...opts,
+    aliasDivider: opts.aliasDivider ? opts.aliasDivider : '|',
+    pageResolver: opts.pageResolver ? opts.pageResolver : (name) => {
+      let heading = ''
+      if (name.match(/#/)) {
+        [, heading] = name.split('#')
+        name = name.replace(`#${heading}`, '')
+      }
+      if (opts.markdownFolder) {
+        const url = opts.permalinks.find(p => p === name || (p.split('/').pop() === name && !opts.permalinks.includes(p.split('/').pop())))
+        if (url) {
+          if (heading) return [`${url}#${heading}`.replace(/ /g, '-').toLowerCase()]
+          return [url.replace(/ /g, '-').toLowerCase()]
+        }
+      }
+      return [name.replace(/ /g, '-').toLowerCase()]
+    },
+    permalinks: opts.markdownFolder ? getFiles(opts.markdownFolder).map(file => file.replace('.md', '')) : opts.permalinks
   }
 
   add('micromarkExtensions', syntax(opts))
