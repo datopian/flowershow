9aa3e0edf864fc55e9ae0e29552e10cd42e0e414
khalilcodes <khalilali.j@gmail.com>
2022-07-11 13:10:37 +0300

    [content,tests,#14]: add wikilinks documentation and tests

--- templates/default/tests/layouts.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require("@playwright/test")
-
-test.describe('Layout tests', () => {
-    
-    // Test if the default page layout comes from layouts/docs.js
-    test('Default layout', async ({ page }) => {
-        
-    })
-})
