23609911e0fc47991bbc547c18f2aea506b5fd7b
olayway <ola@yway.pl>
2022-07-18 16:54:59 +0200

    [site/content][m]: minor docs adjustments

--- templates/default/components/Demo.js
+++ templates/default/components/Demo.js
@@ -1,8 +1,8 @@
 export default function DemoComponent(props) {
 	return (
-		<div className="max-w-2xl mx-auto my-12 px-4">
-			<h3 className="text-center text-3xl mb-4 font-extrabold">Demo component with props data</h3>
-			<p className="mb-4">Below data is coming from props ie., <b>{"<DemoComponent data={testData} />"}</b></p>
+		<div className="max-w-2xl mx-auto my-12 px-4 border-2 border-slate-400 rounded-md">
+			<h3 className="text-center text-3xl mb-4 font-extrabold">I'm a demo component</h3>
+		  <p className="mb-4">Below data is coming from my "data" prop</p>
 			<pre className="bg-gray-900 p-4 text-white">{JSON.stringify(props.data, null, 2)}</pre>
 		</div>
 	)
