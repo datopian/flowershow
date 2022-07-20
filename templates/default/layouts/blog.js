cd85ceada9bb8a7cec63539f69db61dea9d9da54
olayway <ola@yway.pl>
2022-07-20 10:06:59 +0200

    [site/content][m]: docs on layouts and more

--- templates/default/layouts/blog.js
+++ templates/default/layouts/blog.js
@@ -1,10 +1,13 @@
 export default function BlogLayout ({ children, frontMatter }) {
-    const { date, title } = frontMatter
+    const { date, title, authors } = frontMatter
     return (
       <article className="text-center font-serif prose prose-invert mx-auto p-6">
         <header>
-          <div className="mb-6">{title && <h1>{title}</h1>}</div>
-          <div className="mb-6">{date && <p className="text-bold text-teal-500">{(new Date(date)).toLocaleDateString()}</p>}</div>
+          <div className="mb-6">
+            {title && <h1>{title}</h1>}
+            {authors && <p className="text-bold my-0">{authors}</p>}
+            {date && <p className="text-bold text-teal-500 my-0">{(new Date(date)).toLocaleDateString()}</p>}
+          </div>
         </header>
         <section>
           {children}
