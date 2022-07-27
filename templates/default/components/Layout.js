982582684fc203c76c5846de90d37dc30673ebcf
olayway <ola@yway.pl>
2022-07-27 17:35:45 +0200

    [components/Layout][s]: Nav positioning fix

--- templates/default/components/Layout.js
+++ templates/default/components/Layout.js
@@ -14,24 +14,26 @@ export default function Layout({ children, title='' }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Header />
-      <main className="dark:bg-slate-900">
-        {children}
-      </main>
-      <footer className="dark:bg-slate-900 prose dark:prose-invert w-full max-w-none flex items-center justify-center w-full h-24 border-t">
-        <p className="flex items-center justify-center">
-          Created by
-          <a
-            href={siteConfig.authorUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center no-underline"
-          >
-            <img src={siteConfig.authorLogo} alt={siteConfig.author} className="ml-2 h-6 block" />
-            {siteConfig.author}
-          </a>
-        </p>
-      </footer>
+      <div className="relative min-h-screen pb-24 dark:bg-slate-900">
+        <Header />
+        <main>
+          {children}
+        </main>
+        <footer className="absolute bottom-0 dark:bg-slate-900 prose dark:prose-invert max-w-none flex items-center justify-center w-full h-24 border-t">
+          <p className="flex items-center justify-center">
+            Created by
+            <a
+              href={siteConfig.authorUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center no-underline"
+            >
+              <img src={siteConfig.authorLogo} alt={siteConfig.author} className="ml-2 h-6 block" />
+              {siteConfig.author}
+            </a>
+          </p>
+        </footer>
+      </div>
     </>
   )
 }
