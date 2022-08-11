7fe758d46416bed6786390d60559a5169eaef57d
olayway <ola@yway.pl>
2022-08-11 15:27:04 +0200

    [content/test][s]: test page for component imports

--- templates/default/components/TestComponent.jsx
+++ templates/default/components/TestComponent.jsx
@@ -1,17 +1,17 @@
-import { Test } from './Test.jsx'
+import { TestChild } from './TestChild.jsx'
 
-export const Test2 = ({simpleList}) => {
+export const TestComponent = ({ simpleList }) => {
   return (
     <div>
       <p>I'm a custom react component imported in this page</p>
       <div>
-        <p>Here is my child component "Test.js"</p>
+        <p>Here is my child component:</p>
         <div className="border-2 border-yellow-300">
-          <Test/>
+          <TestChild />
         </div>
       </div>
       <div>
-        <p>And here is data passed through props:</p>
+        <p>And here is data passed to me through props:</p>
         <div className="border-2 border-green-300">
           <ul>
             { simpleList.map((x, i) => <li key={i.toString()}>{x}</li>) }
