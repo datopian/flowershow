30816d70c47819315a38f00f1006984d20b7c1d1
khalilcodes <khalilali.j@gmail.com>
2022-08-12 14:35:25 +0400

    [contentlayer][s]: configure mermaid pre tags

--- templates/default/contentlayer.config.js
+++ templates/default/contentlayer.config.js
@@ -5,6 +5,8 @@ import rehypePrismPlus from "rehype-prism-plus";
 import wikiLinkPlugin from "remark-wiki-link-plus";
 import remarkMath from "remark-math";
 import rehypeMathjax from 'rehype-mathjax';
+import codeExtra from "remark-code-extra";
+import mermaidPlugin from "mdx-mermaid";
 
 const sharedFields = {
   title: { type: "string" },
@@ -57,7 +59,45 @@ export default makeSource({
     remarkPlugins: [ 
       remarkGfm,
       remarkMath,
-      [ wikiLinkPlugin, { markdownFolder: siteConfig.content } ]
+      [wikiLinkPlugin, { markdownFolder: siteConfig.content }],
+      /** Using the code extra plugin from https://github.com/s0/remark-code-extra
+       *  to create new mermaid pre tags to use with mdx-mermaid.
+       *  rehypePrismPlus plugin modifies the pre tags and due to this mdx-mermaid
+       *  component cannot receive the values in the required format.
+       *  Refer issue https://github.com/flowershow/flowershow/issues/12 for more info.
+      */
+      [codeExtra, {
+        transform: (node) => {
+          if (node.type == "code" && node.lang == "mermaid") {
+            // reset values else rehype-prism-plus throws error
+            node.type = ""
+            node.lang = ""
+            return ({
+              // create new pre tag element here for mermaid
+              after: [{
+                type: "element",
+                tagName: "pre",
+                properties: {
+                  className: "code-mermaid"
+                },
+                children: [{
+                  type: "text",
+                  value: node.value
+                }]
+              }],
+              // remove the pre tag element created by rehype-prism-plus
+              // otherwise both will be displayed
+              transform: n => {
+                const preElem = n.data.hChildren.find(el => el.tagName === "pre")
+                const index = n.data.hChildren.indexOf(preElem)
+                n.data.hChildren.splice(index, 1)
+              }
+            })
+          } else {
+            return null
+          }
+        }
+      }],
     ],
     rehypePlugins: [
       rehypeMathjax,
