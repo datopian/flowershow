c681cb8369f9ace84a147dbe3bc9d591b4c52866
khalilcodes <khalilali.j@gmail.com>
2022-08-17 12:54:13 +0400

    [tests][s]: comment out local tests

--- templates/default/tests/example.spec.js
+++ templates/default/tests/example.spec.js
@@ -2,9 +2,9 @@ const { test, expect } = require('@playwright/test')
 
 /* First pass on test setup */
 
-test('Basic test', async ({ page }) => {
-  // Start from the index page
-  await page.goto('/');
-  // The page should contain an h1 with the title 'Flowershow Default Template'
-  await expect(page.locator('title')).toContainText('Flowershow');
-})
+// test('Basic test', async ({ page }) => {
+//   // Start from the index page
+//   await page.goto('/');
+//   // The page should contain an h1 with the title 'Flowershow Default Template'
+//   await expect(page.locator('title')).toContainText('Flowershow');
+// })
