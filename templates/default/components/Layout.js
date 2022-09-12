e46b62c29a01d954a5e84c7741508728d3d1816a
philippedupreez <philippedupreez79@gmail.com>
2022-09-12 20:15:02 +0200

    move edit link to layout and change frontmatter edit link logic

--- templates/default/components/Layout.js
+++ templates/default/components/Layout.js
@@ -5,10 +5,17 @@ import siteConfig from '../config/siteConfig';
 import Header from './Nav';
 
 export default function Layout({ children, title = '' }) {
+  const { editLink, _raw } = children.props;
+   /* Edit links will only appear if enabled in siteConfig. If set to true, all pages will show link by default.
+      Files can include frontmatter, editLink: false, to not show specific page link */
+  const editUrl = siteConfig.repoRoot + siteConfig.repoEditPath + _raw.sourceFilePath;
   return (
     <>
       <Head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💐</text></svg>" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💐</text></svg>"
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
@@ -16,6 +23,31 @@ export default function Layout({ children, title = '' }) {
         <Header />
         <main>
           {children}
+          {editUrl && siteConfig.editLinkShow && editLink && (
+            <div className="docs prose dark:prose-invert mx-auto p-6">
+              <a
+                className="flex no-underline font-semibold"
+                href={editUrl}
+                target="_blank">
+                Edit this page
+                <span className="mx-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                    />
+                  </svg>
+                </span>
+              </a>
+            </div>
+          )}
         </main>
         <footer className="absolute bottom-0 dark:bg-slate-900 prose dark:prose-invert max-w-none flex flex-col items-center justify-center w-full h-auto pt-10 pb-20">
           <div className="flex w-full flex-wrap justify-center">
@@ -25,8 +57,7 @@ export default function Layout({ children, title = '' }) {
                   key={item.name}
                   href={item.href}
                   className="inline-flex items-center mx-4 px-1 pt-1 font-regular hover:text-slate-300 no-underline"
-                  aria-current={item.current ? 'page' : undefined}
-                >
+                  aria-current={item.current ? 'page' : undefined}>
                   {item.name}
                 </a>
               </Link>
@@ -38,10 +69,14 @@ export default function Layout({ children, title = '' }) {
               href={siteConfig.authorUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center no-underline"
-            >
-              {siteConfig.authorLogo
-                 && <img src={siteConfig.authorLogo} alt={siteConfig.author} className="my-0 h-6 block" />}
+              className="flex items-center no-underline">
+              {siteConfig.authorLogo && (
+                <img
+                  src={siteConfig.authorLogo}
+                  alt={siteConfig.author}
+                  className="my-0 h-6 block"
+                />
+              )}
               {siteConfig.author}
             </a>
           </p>
