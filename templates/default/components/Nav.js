776fb256b2188f9990f68256100fdf7e39fcbfad
Rufus Pollock <rufus.pollock@datopian.com>
2022-09-14 11:21:26 +0200

    [#111,tmpl/default,nav][s]: even cleaner refactor of work in 4dca69839eab4c1ece9c6300a2aa643a826d2124.

--- templates/default/components/Nav.js
+++ templates/default/components/Nav.js
@@ -15,25 +15,27 @@ function GitHubIcon(props) {
 }
 
 function NavbarTitle() {
-  // include option to define different text from title (if needed) in config
-  const title = siteConfig.navbarTitle?.text || siteConfig.title
+  const chunk = (
+    <>
+      {siteConfig.navbarTitle?.logo && <img src={siteConfig.navbarTitle.logo} alt={siteConfig.navbarTitle.text} className="w-9 h-9 mr-1 fill-white" />}
+      {siteConfig.navbarTitle?.text}
+      {siteConfig.navbarTitle?.version &&
+        (
+          <div className="mx-2 rounded-full border border-slate-500 py-1 px-3 text-xs text-slate-500">
+            {siteConfig.navbarTitle?.version}
+          </div>
+        )
+      }
+    </>
+  )
 
   return (
-    <>
-      <Link href="/" aria-label="Home page">
-        <a className="flex items-center font-extrabold text-xl sm:text-2xl text-slate-900 dark:text-white">
-          {siteConfig.navbarTitle?.logo && <img src={siteConfig.navbarTitle.logo} alt={title} className="w-9 h-9 mr-1 fill-white" />}
-          {title}
-          {siteConfig.navbarTitle?.version &&
-            (
-              <div className="mx-2 rounded-full border border-slate-500 py-1 px-3 text-xs text-slate-500">
-                {siteConfig.navbarTitle?.version}
-              </div>
-            )
-          }
-        </a>
-      </Link>
-    </>
+    <Link href="/" aria-label="Home page">
+      <a className="flex items-center font-extrabold text-xl sm:text-2xl text-slate-900 dark:text-white">
+        {siteConfig.navbarTitle && chunk}
+        {!siteConfig.navbarTitle && siteConfig.title}
+      </a> 
+    </Link>
   )
 }
 
