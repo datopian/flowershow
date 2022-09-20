461c9cb61af117bfa1495da0fa268b79b1460fd2
olayway <ola@yway.pl>
2022-09-20 22:39:58 +0200

    test

--- templates/default/components/Layout.js
+++ templates/default/components/Layout.js
@@ -4,6 +4,7 @@ import siteConfig from '../config/siteConfig';
 import Header from './Nav';
 
 export default function Layout({ children }) {
+  const test = true;
   const { editLink, _raw } = children.props;
   /* if editLink is not set in page frontmatter, link bool value will depend on siteConfig.editLinkShow */
   const editUrl = siteConfig.repoRoot + siteConfig.repoEditPath + _raw?.sourceFilePath;
