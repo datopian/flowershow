9697978e750e8937db846362c4ff2cd403174fd0
olayway <ola@yway.pl>
2022-09-20 22:01:45 +0200

    fix img tags

--- site/components/WhatIsFlowershow.jsx
+++ site/components/WhatIsFlowershow.jsx
@@ -10,7 +10,7 @@ export function WhatIsFlowershow() {
             <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]">
             </div>
             <div className="relative overflow-hidden rounded-xl p-6">
-              <img src="/assets/images/obsidian_icon.png" className="h-12 w-auto" />
+              <img src="/assets/images/obsidian_icon.png" alt="" className="h-12 w-auto" />
               <h2 className="mt-4 font-display text-base text-slate-900 dark:text-white">Crafted for Obsidian</h2>
               <p className="mt-1 text-sm text-slate-700 dark:text-slate-400">Works with your existing Obsidian notes out of the box. No need to modify the syntax or change file layout. Not an obsidian fan? Flowershow will work with any CommonMark or GFM markdown files.</p>
             </div>
@@ -20,7 +20,7 @@ export function WhatIsFlowershow() {
             <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]">
             </div>
             <div className="relative overflow-hidden rounded-xl p-6">
-              <img src="/assets/images/park.png" className="h-12 w-auto" />
+              <img src="/assets/images/park.png" alt="" className="h-12 w-auto" />
               <h2 className="mt-4 font-display text-base text-slate-900 dark:text-white">Elegant & Functional</h2>
               <p className="mt-1 text-sm text-slate-700 dark:text-slate-400">Clean and elegant default theme with dark and light mode, hero, navbar, backlinks section and support for internal links, backlinks, math, mermaid, callouts and more.</p>
             </div>
@@ -31,7 +31,7 @@ export function WhatIsFlowershow() {
             <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]">
             </div>
             <div className="relative overflow-hidden rounded-xl p-6">
-              <img src="/assets/images/farming.png" className="h-12 w-auto" />
+              <img src="/assets/images/farming.png" alt="" className="h-12 w-auto" />
               <h2 className="mt-4 font-display text-base text-slate-900 dark:text-white">Customize and extend (if that’s your thing!)</h2>
               <p className="mt-1 text-sm text-slate-700 dark:text-slate-400">Want to spice things up? Flowershow is easy to extend, tweak and customize and is built on standard, modern web stack – React, NextJS and Tailwind.</p>
             </div>
@@ -41,7 +41,7 @@ export function WhatIsFlowershow() {
             <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]">
             </div>
             <div className="relative overflow-hidden rounded-xl p-6">
-              <img src="/assets/images/data.png" className="h-12 w-auto" />
+              <img src="/assets/images/data.png" alt="" className="h-12 w-auto" />
               <h2 className="mt-4 font-display text-base text-slate-900 dark:text-white">Backed by an experienced team</h2>
               <p className="mt-1 text-sm text-slate-700 dark:text-slate-400">We’ve been building content and data driven products for over a decade, and we’re passionate about sharing knowledge and ideas with others.<br/>We love markdown as much as we love open-source (which is a lot!).</p>
             </div>
