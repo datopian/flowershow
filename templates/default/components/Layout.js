2575b8e0c68cc77fd2778631c97eb4ddf27de964
olayway <ola@yway.pl>
2022-09-22 12:54:06 +0200

    asf

--- templates/default/components/Layout.js
+++ templates/default/components/Layout.js
@@ -4,7 +4,7 @@ import siteConfig from '../config/siteConfig';
 import Header from './Nav';
 
 export default function Layout({ children }) {
-  const test = false;
+  const test = true;
   const { editLink, _raw } = children.props;
   /* if editLink is not set in page frontmatter, link bool value will depend on siteConfig.editLinkShow */
   const editUrl = siteConfig.repoRoot + siteConfig.repoEditPath + _raw?.sourceFilePath;
@@ -28,8 +28,7 @@ export default function Layout({ children }) {
                 className="flex no-underline font-semibold justify-center"
                 href={editUrl}
                 target="_blank"
-                rel="noopener noreferrer"
-              >
+                rel="noopener noreferrer">
                 Edit this page
                 <span className="mx-1">
                   <svg
