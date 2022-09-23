2676f4bab570b372d0288891dfa810fb45644c17
olayway <ola@yway.pl>
2022-09-23 12:50:30 +0200

    sort imports

--- templates/default/components/Nav.jsx
+++ templates/default/components/Nav.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
 import Link from "next/link";
-import { MobileNavigation } from "./MobileNavigation";
+import { useEffect, useState } from "react";
+
 import { siteConfig } from "../config/siteConfig";
+import { MobileNavigation } from "./MobileNavigation";
+import { NavItem } from "./NavItem";
 import { Search } from "./Search";
 import { ThemeSelector } from "./ThemeSelector";
-import { NavItem } from "./NavItem";
 
 function GitHubIcon(props) {
   return (
--- templates/default/components/ThemeSelector.jsx
+++ templates/default/components/ThemeSelector.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 import { siteConfig } from "../config/siteConfig";
 
 export function ThemeSelector() {
