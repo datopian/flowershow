f6f3d137b0aa36e49373f3868a67181c95f90878
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2022-10-20 11:37:48 +0200

    [CLI,content][s]: alpha release updates
    * CLI
      - build-static command added
      - ability to select assets folder from the list of available subfolders
      - ability to choose not to use assets folder
      - E2E adjustments
    * Content
      - enhanced frontpage self-publish and publish sections
      - enhanced self-publish tutorial

--- packages/cli/lib/Installer.js
+++ packages/cli/lib/Installer.js
@@ -10,17 +10,14 @@ import inquirer from 'inquirer';
 
 import { exit, error, log, success, logWithSpinner, stopSpinner, pauseSpinner, resumeSpinner } from './utils/index.js';
 
+import { FLOWERSHOW_FOLDER_NAME } from './const.js';
 
-import { FLOWERSHOW_RELATIVE_PATH } from './const.js';
 
 export default class Creator {
-  constructor(context, template = 'default') {
+  constructor(context, targetDir, template = 'default') {
     this.context = context;
+    this.targetDir = targetDir;
     this.template = template; // tb configurable via command option in the future
-  }
-
-  get flowershowDir() {
-    return path.resolve(this.context, FLOWERSHOW_RELATIVE_PATH);
   }
 
   get templateRepo() {
@@ -29,69 +26,115 @@ export default class Creator {
   }
 
   async install(options) {
-    const { context, flowershowDir, templateRepo } = this;
+    const { context, targetDir, templateRepo } = this;
+    const flowershowDir = path.resolve(targetDir, FLOWERSHOW_FOLDER_NAME)
 
-    logWithSpinner({ symbol: '🌷', msg: `Installing Flowershow template in ${chalk.magenta(flowershowDir)}...` });
-
+    let existsAction;
     if (fs.existsSync(flowershowDir)) {
-      pauseSpinner();
-
-      const { action } = await inquirer.prompt([
+      let { action } = await inquirer.prompt([
         {
           name: 'action',
           type: 'list',
-          message: `Flowershow template is already installed in directory ${chalk.magenta(context)}. What do you want to do?:`,
+          message: `Flowershow template is already installed in directory ${chalk.magenta(targetDir)}. What do you want to do?:`,
           choices: [
             { name: 'Overwrite', value: 'overwrite' },
             // { name: 'Merge', value: 'merge' },
-            { name: 'Cancel', value: false }
+            { name: 'Cancel', value: null }
           ]
         }
       ])
 
       if (!action) {
-        return
-      } else {
-        fs.rmSync(flowershowDir, { recursive: true, force: true });
+        exit(0)
       }
-      resumeSpinner();
+      existsAction = action;
     }
-
-    // clone flowershow template
-    try {
-      const emitter = degit(templateRepo);
-      await emitter.clone(flowershowDir);
-    } catch {
-      // TODO better error message
-      error(`Failed to clone Flowershow template.`)
-      exit(1);
-    }
-
-    // symlink content folder
-    pauseSpinner();
 
     let { contentPath } = await inquirer.prompt([
       {
         name: 'contentPath',
         type: 'input',
-        message: 'Path to the folder with your content files',
+        message: 'Path to the folder with your markdown files',
         validate(input) {
-          const contentPathAbsolute = path.resolve(context, input);
-          if (!fs.existsSync(contentPathAbsolute)) {
-            error(`Directory ${contentPathAbsolute} does not exist.`);
+          const contentDir = path.resolve(context, input);
+          if (!fs.existsSync(contentDir)) {
+            error(`Directory ${contentDir} does not exist.`);
             exit(1);
           }
-          resumeSpinner();
           return true;
         }
       }
     ])
 
-    contentPath = path.resolve(context, contentPath);
 
+    const contentDir = path.resolve(context, contentPath);
+    const assetFolderChoices = fs.readdirSync(contentDir, { withFileTypes: true })
+                                 .filter(d => d.isDirectory())
+                                 .map(d => ({ name: d.name, value: d.name }))
+
+    let assetsFolder = 'none';
+
+    if (!assetFolderChoices.length) {
+      const { foldersAction } = await inquirer.prompt([
+        {
+          name: 'foldersAction',
+          type: 'list',
+          message: 'There are no subfolders in your content folder, that could be used as assets folder',
+          choices: [
+            { name: "I don't need assets folder", value: 'none' },
+            { name: 'Cancel', value: null }
+          ]
+        }
+      ])
+
+      assetsFolder = foldersAction;
+
+    } else {
+
+      const { assets } = await inquirer.prompt([
+        {
+          name: 'assets',
+          type: 'list',
+          message: 'Select a folder with your assets (attachments)',
+          choices: [
+            ...assetFolderChoices,
+            new inquirer.Separator(),
+            { name: "I don't need assets folder", value: 'none' },
+            { name: 'Cancel', value: null }
+          ]
+        }
+      ])
+
+      assetsFolder = assets;
+    }
+
+    if (!assetsFolder) {
+      exit(0)
+    }
+
+    // install flowershow template
+    logWithSpinner({ symbol: '🌷', msg: `Installing Flowershow template in ${chalk.magenta(flowershowDir)}...` });
+
+    if (existsAction === 'overwrite') {
+      fs.rmSync(flowershowDir, { recursive: true, force: true });
+    }
+
+    try {
+      const emitter = degit(templateRepo);
+      await emitter.clone(flowershowDir);
+    } catch {
+      error(`Failed to install Flowershow template in ${flowershowDir}.`)
+      exit(1);
+    }
+
+    // update content and assets symlinks
     fs.unlinkSync(`${flowershowDir}/content`);
-    fs.symlinkSync(contentPath, `${flowershowDir}/content`);
+    fs.symlinkSync(contentDir, `${flowershowDir}/content`);
 
+    fs.unlinkSync(`${flowershowDir}/public/assets`);
+    if (assetsFolder !== 'none') {
+      fs.symlinkSync(path.resolve(contentDir, assetsFolder), `${flowershowDir}/public/${assetsFolder}`);
+    }
 
     // // if there is no index.md file, create one
     if (!fs.existsSync(`${contentPath}/index.md`)) {
@@ -104,30 +147,6 @@ export default class Creator {
       fs.writeFile(`${contentPath}/config.js`, '{}', { flag: 'a' }, err => {});
     }
 
-    // symlink assets folder
-    pauseSpinner();
-
-    const { assetsFolder } = await inquirer.prompt([
-      {
-        name: 'assetsFolder',
-        type: 'input',
-        message: 'Name of your assets (attachements) folder',
-        validate(input) {
-          const assetsPathAbsolute = path.resolve(contentPath, input);
-          if (!fs.existsSync(assetsPathAbsolute)) {
-            error(`Directory ${assetsPathAbsolute} does not exist.`);
-            exit(1);
-          }
-          resumeSpinner();
-          return true;
-        }
-      }
-    ])
-
-    fs.unlinkSync(`${flowershowDir}/public/assets`);
-    fs.symlinkSync(path.resolve(contentPath, assetsFolder), `${flowershowDir}/public/assets`);
-
-
     // install flowershow dependencies
     logWithSpinner({ symbol: '🌸', msg: `Installing Flowershow dependencies...` });
 
@@ -137,7 +156,7 @@ export default class Creator {
       log(stdout);
       log(stderr);
       stopSpinner();
-      success("Successfuly installed Flowershow template!")
+      success("Successfuly installed Flowershow!")
     } catch (err) {
       error(
         `Installing dependencies failed: ${err.message}`
--- packages/cli/lib/buildStatic.js
+++ packages/cli/lib/buildStatic.js
@@ -3,25 +3,11 @@ import path from 'path';
 import { execa } from 'execa';
 
 import { exit, error, log } from './utils/index.js';
+import { FLOWERSHOW_FOLDER_NAME } from './const.js';
 
-
-import { FLOWERSHOW_RELATIVE_PATH } from './const.js';
 
 export default async function buildStatic(dir) {
-  const flowershowDir = path.resolve(dir, FLOWERSHOW_RELATIVE_PATH);
-
-  // check if flowershow is installed
-  if (!fs.existsSync(flowershowDir)) {
-    error(`Directory ${flowershowDir} does not exist.`)
-    exit(1);
-  }
-
-  // check if flowershow is built
-  if (!fs.existsSync(`${flowershowDir}/.next`)) {
-    // TODO add prompt to allow for building it at this point or do this by default without a prompt ?
-    error("Your Flowershow site is not built yet. Run `npx flowershow build` first.")
-    exit(1);
-  }
+  const flowershowDir = path.resolve(dir, FLOWERSHOW_FOLDER_NAME);
 
   const subprocess = execa('npm', [ 'run', 'export' ], { cwd: flowershowDir });
 
