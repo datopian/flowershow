ccba02b5495624a74d5389ac3e72babc39a17bb0
olayway <ola@yway.pl>
2022-10-25 00:02:32 +0200

    [m]: in-component usage of contentlayer data

--- templates/default/pages/[[...slug]].jsx
+++ templates/default/pages/[[...slug]].jsx
@@ -3,9 +3,10 @@ import { allDocuments } from "contentlayer/generated";
 import { useMDXComponent } from "next-contentlayer/hooks";
 
 import { MdxPage } from "../components/MDX";
+import { getPageData } from "../lib/getPageData";
 
-export default function Page({ body, ...rest }) {
-  const mdxComponent = useMDXComponent(body.code);
+export default function Page({ body, data, ...rest }) {
+  const mdxComponent = useMDXComponent(body.code, { ...data });
   const frontMatter = { ...rest };
   return <MdxPage mdxComponent={mdxComponent} frontMatter={frontMatter} />;
 }
@@ -14,7 +15,8 @@ export async function getStaticProps({ params }) {
   // params.slug is undefined for root index page
   const urlPath = params.slug ? params.slug.join("/") : "";
   const page = allDocuments.find((p) => p.url_path === urlPath);
-  return { props: page };
+  const data = await getPageData(page.data);
+  return { props: { ...page, data } };
 }
 
 export async function getStaticPaths() {
