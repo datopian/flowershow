8335c821b1a747c60a703c45fb877789d657a154
khalilcodes <khalilali.j@gmail.com>
2022-11-03 10:56:32 +0400

    [components][m]: replace algolia search component

---
+++ templates/default/components/search/AlgoliaModal.jsx
@@ -0,0 +1,3 @@
+// Re-export as default to help the bundler resolve the packages
+// For some reason tsup is not able to detect when using named export
+export { DocSearchModal } from "@docsearch/react";
