5fff379eb648e961a99d6b689c67ed14dfc294da
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2022-11-08 12:56:36 +0100

    Monorepo setup (#241)
    Changes:
    
    - adds Nx to the repo,
    - configures Nx Cloud distributed caching
    - moves templates/default to packages/template
    - creates packages/template-e2e package for template's e2e tests
    - moves playwright dependency to the root, so that it can be used for other projects in the future
    - changes the bash setup script to use the temporary directory, where the template and content fixture are copied and symlinked, so that the setup script doesn't ever touch the original template codebase and we don't need to clean up (i.e. revert symlinks to the original ones) after the tests have run,
    - moves lint-staged, husky, changesets and playwright to the root
    - adds basic info related to Nx and changesets in README.dev.md
    - adds a note about Nx basics in content/notes/Nx.md
    - renames the CLI to namespaced @flowershow/cli
    - recommended package manager: pnpm

---
+++ packages/cli/src/lib/preview.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { execa } from "execa";
+
+import { exit, error } from "./utils/index.js";
+
+import { FLOWERSHOW_FOLDER_NAME } from "./const.js";
+
+export default async function preview(dir) {
+  const flowershowDir = path.resolve(dir, FLOWERSHOW_FOLDER_NAME);
+
+  // check if flowershow is installed
+  if (!fs.existsSync(flowershowDir)) {
+    error(`Directory ${flowershowDir} does not exist.`);
+    exit(1);
+  }
+  const subprocess = execa("npm", ["run", "dev"], { cwd: flowershowDir });
+
+  subprocess.stdout.pipe(process.stdout);
+}
