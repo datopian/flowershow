71f0130ce383b24e275e678533dce9e90217c396
olayway <ola@yway.pl>
2022-11-11 14:29:29 +0100

    [template/components][xs]: fix formatting

--- packages/template/scripts/search.mjs
+++ packages/template/scripts/search.mjs
@@ -1,21 +1,25 @@
-import { writeFileSync } from "fs"
-import { allDocuments } from "../.contentlayer/generated/index.mjs"
+import { writeFileSync } from "fs";
+import { allDocuments } from "../.contentlayer/generated/index.mjs";
 
 const omit = (obj = {}, keys = []) => {
-  const result = Object.assign({}, obj)
+  const result = Object.assign({}, obj);
   keys.forEach((key) => {
-    delete result[key]
-  })
-  return result
-}
+    delete result[key];
+  });
+  return result;
+};
 
-const coreContent = allDocuments.map(doc => omit({ 
-  ...doc,
-  sourceDir: doc._raw.sourceFileDir === "."
-    ? null
-    : doc._raw.sourceFileDir 
-  }, 
-  ['body', '_raw', '_id']
-)).filter(doc => !doc.isDraft)
+const coreContent = allDocuments
+  .map((doc) =>
+    omit(
+      {
+        ...doc,
+        sourceDir:
+          doc._raw.sourceFileDir === "." ? null : doc._raw.sourceFileDir,
+      },
+      ["body", "_raw", "_id"]
+    )
+  )
+  .filter((doc) => !doc.isDraft);
 
-writeFileSync("public/search.json", JSON.stringify(coreContent))
+writeFileSync("public/search.json", JSON.stringify(coreContent));
