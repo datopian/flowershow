dd3fba5c68801dfd492edbda6dc9a1e0016dab8b
olayway <ola@yway.pl>
2022-11-11 14:29:29 +0100

    [template/components][xs]: fix formatting

--- packages/template/components/search/SearchField.jsx
+++ packages/template/components/search/SearchField.jsx
@@ -19,7 +19,8 @@ export function SearchField(props) {
           : "hidden sm:flex sm:justify-start md:h-auto md:w-auto xl:w-full max-w-[380px] shrink xl:rounded-lg xl:py-2.5 xl:pl-4 xl:pr-3.5 md:text-sm xl:ring-1 xl:ring-slate-200 xl:hover:ring-slate-300 dark:xl:bg-slate-800/75 dark:xl:ring-inset dark:xl:ring-white/5 dark:xl:hover:bg-slate-700/40 dark:xl:hover:ring-slate-500"
       }
     `}
-      onClick={onOpen}>
+      onClick={onOpen}
+    >
       <SearchIcon className="h-5 w-5 flex-none fill-slate-400 group-hover:fill-slate-500 dark:fill-slate-500 md:group-hover:fill-slate-400" />
       <span
         className={`
@@ -29,7 +30,8 @@ export function SearchField(props) {
             ? "w-full not-sr-only text-left ml-2"
             : "hidden xl:block sr-only md:not-sr-only md:ml-2"
         }
-      `}>
+      `}
+      >
         Search docs
       </span>
       {modifierKey && (
@@ -40,7 +42,8 @@ export function SearchField(props) {
               ? "hidden"
               : "ml-auto font-medium text-slate-400 dark:text-slate-500 hidden xl:block"
           }
-        `}>
+        `}
+        >
           <kbd className="font-sans">{modifierKey}</kbd>
           <kbd className="font-sans">K</kbd>
         </kbd>
