258f4575393b70aef193b2582dc1952e9fad1d9c
olayway <ola@yway.pl>
2022-11-11 14:56:10 +0100

    [packages/cli][xs]: rename build-static to export

--- packages/cli/src/lib/buildExport.js
+++ packages/cli/src/lib/buildExport.js
@@ -3,7 +3,7 @@ import { execa } from "execa";
 
 import { FLOWERSHOW_FOLDER_NAME } from "./const.js";
 
-export default async function buildStatic(dir) {
+export default async function buildExport(dir) {
   const flowershowDir = path.resolve(dir, FLOWERSHOW_FOLDER_NAME);
 
   const subprocess = execa("npm", ["run", "export"], { cwd: flowershowDir });
