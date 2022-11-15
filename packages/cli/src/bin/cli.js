b3ba84bf95b790674560c4828855e7eafc5539f6
olayway <ola@yway.pl>
2022-11-15 20:48:19 +0100

    [packages/cli][s]: send event to ua on uncaugth exception

--- packages/cli/src/bin/cli.js
+++ packages/cli/src/bin/cli.js
@@ -1,36 +1,48 @@
 #!/usr/bin/env node
-import os from "os";
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
-import { warn, sendEvent } from "../lib/utils/index.js";
+import { warn, exit, sendEvent } from "../lib/utils/index.js";
 import { Command } from "commander";
-import inquirer from "inquirer";
 
-if (os.platform() === "win32") {
+// TODO check current vs required node version (package.json engines)
+// const requiredNodeVersion = require("../package.json").engines.node;
+
+const { version: cli } = require("../../package.json");
+const { version: node, platform, argv } = process;
+
+if (platform === "win32") {
   warn(
     "This may not work as expected. You're trying to run Flowreshow CLI on Windows, which is not thoroughly tested. Please submit an issue if you encounter any problems: https://github.com/flowershow/flowershow/issues"
   );
 }
 
-// TODO check current vs required node version (package.json engines)
-// const requiredNodeVersion = require("../package.json").engines.node;
+const [, , cmd, ...args] = argv;
+sendEvent({
+  event: "cli-usage",
+  action: cmd,
+  meta: {
+    args,
+    cli,
+    node,
+    platform,
+  },
+});
 
-// ask the user for permission to send anonymous telemetry
-if (process.env.FLOWERSHOW !== "dev") {
-  const { allow } = await inquirer.prompt([
-    {
-      name: "allow",
-      type: "confirm",
-      message:
-        "If you don't mind, we'd like to collect very basic anonymous telemetry that will help us improve this tool. Is that ok?",
+process.on("uncaughtException", () => {
+  sendEvent({
+    event: "cli-error",
+    action: cmd,
+    meta: {
+      args,
+      cli,
+      node,
+      platform,
     },
-  ]);
+  });
+  exit(1);
+});
 
-  if (allow) {
-    const [, , cmd, ...args] = process.argv;
-    sendEvent(cmd, args);
-  }
-}
+// CLI commands
 
 const program = new Command();
 
--- packages/cli/src/lib/utils/sendEvent.js
+++ packages/cli/src/lib/utils/sendEvent.js
@@ -1,17 +1,7 @@
 import ua from "universal-analytics";
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
 
-const { version } = require("../../../package.json");
+const visitor = ua("UA-235099461-1");
 
-export function sendEvent(cmd, args) {
-  const visitor = ua("UA-235099461-1");
-  visitor
-    .event(
-      "cli-usage-by-os-and-version",
-      process.platform,
-      JSON.stringify({ cli: version, node: process.version })
-    )
-    .send();
-  visitor.event("cli-usage-by-command", cmd, args).send();
+export function sendEvent({ event, action, meta }) {
+  visitor.event(event, action, JSON.stringify(meta)).send();
 }
