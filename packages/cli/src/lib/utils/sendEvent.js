e05096bd85fb5fa91376f76e7943a015f917475b
olayway <ola@yway.pl>
2022-11-15 10:51:04 +0100

    [packages/cli][xs]: track node version

--- packages/cli/src/lib/utils/sendEvent.js
+++ packages/cli/src/lib/utils/sendEvent.js
@@ -6,10 +6,12 @@ const { version } = require("../../../package.json");
 
 export function sendEvent(cmd, args) {
   const visitor = ua("UA-235099461-1");
-  // Track which version is run and on which OS:
   visitor
-    .event("cli-usage-by-os-and-version", process.platform, version)
+    .event(
+      "cli-usage-by-os-and-version",
+      process.platform,
+      JSON.stringify({ cli: version, node: process.version })
+    )
     .send();
-  // Event category is 'cli', action is the command and label is all arguments:
-  visitor.event("cli-usage-by-command", cmd, args).send();
+  visitor.event("cli-usage-by-command", cmd, JSON.stringify({ args })).send();
 }
