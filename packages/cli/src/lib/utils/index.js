ff6bd2951a91a012bfe99f2c0719da36951b032a
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2022-11-15 20:03:47 +0000

    [packages/cli][m]: basic telemetry with UA

--- packages/cli/src/lib/utils/index.js
+++ packages/cli/src/lib/utils/index.js
@@ -1,6 +1,7 @@
 export { error, info, log, success, warn } from "./logger.js";
 export { exit } from "./exit.js";
 export { isSubdir } from "./isSubdir.js";
+export { sendEvent } from "./sendEvent.js";
 export {
   logWithSpinner,
   stopSpinner,
