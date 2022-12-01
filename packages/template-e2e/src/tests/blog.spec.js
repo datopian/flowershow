91edc4c7b992a872b0cc006121c7d9cf70c35f2b
olayway <ola@yway.pl>
2022-12-01 15:55:26 +0100

    [packages/template-e2e][xs]: removed unneeded test

--- packages/template-e2e/src/tests/blog.spec.js
+++ packages/template-e2e/src/tests/blog.spec.js
@@ -1,28 +1,24 @@
 const { test, expect } = require("@playwright/test");
 const { MarkdownPage } = require("../support/markdown-page");
 
-test.describe.parallel("blog index", () => {
+test.describe.parallel("blog index page", () => {
   test.beforeEach(async ({ page }) => {
     const Page = new MarkdownPage(page);
     await Page.goto("/blog");
     await Page.getData();
   });
 
-  test("blog link1", async ({ page }) => {
+  test("correctly renders a blog post from /blog dir", async ({ page }) => {
     await page.locator("text=Conquer the seven seas").click();
-    await expect(page).toHaveURL("/blog/blogtest1");
+    await expect(page).toHaveURL("/blog/blogtest");
     await expect(page).toHaveTitle("Conquer the seven seas");
   });
 
-  test("blog link2", async ({ page }) => {
-    await page.locator("text=Conquer the lakes").click();
-    await expect(page).toHaveURL("/blog/blogtest2");
-    await expect(page).toHaveTitle("Conquer the lakes");
-  });
-
-  test("blog link3", async ({ page }) => {
+  test("correctly renders a page from outside of /blog dir", async ({
+    page,
+  }) => {
     await page.locator("text=Conquer the puddles").click();
-    await expect(page).toHaveURL("/blogtest3");
+    await expect(page).toHaveURL("/blogtest");
     await expect(page).toHaveTitle("Conquer the puddles");
   });
 });
