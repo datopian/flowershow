c28ef0199b4f3bda2ada36ad146dd4c4e659d88e
olayway <ola@yway.pl>
2022-12-09 01:30:22 +0100

    [packages/template][xs]: rmv unused CardAvatar component

--- packages/template/lib/initialsFromName.js
+++
@@ -1,5 +0,0 @@
-export function initialsFromName(name) {
-  const fullName = name.split(" ");
-  const initials = fullName.shift().charAt(0) + fullName.pop().charAt(0);
-  return initials.toUpperCase();
-}
