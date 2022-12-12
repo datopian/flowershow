f1ed0baade8502598023ab42b71584b5d99a3b5f
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2022-12-12 12:12:28 +0100

    [packages/template][l]: blog authors displayed
    ## Changes
    - `blog` layout with authors displayed below the title
      - if the author page exists, the author will have a link to it
    - e2e tests for blog authors' functionality
    - docs page section about blog authors
    
    ![image](https://user-images.githubusercontent.com/52197250/206603188-e9b54395-ff6f-4185-86ea-c433b045e38b.png)
    
    ## Notes
    Since importing data from contentlayer in layouts results in the error mentioned in the comments below, a temporary workaround used in this PR is to import `allPeople` in the `[...slug]`, finding all the blog authors in there and passing them as an additional frontmatter property - `authorsDetails` - to layout components.

---
+++ packages/template-e2e/src/fixtures/content/people/john-doe.md
@@ -0,0 +1,8 @@
+---
+title: John the Great
+name: John the Great
+---
+
+Avast aft scurvy. Swab mizzenmast stern. Cutlass man-of-war hail-shot.
+Jack gangway tender. Nipperkin hands warp. Ahoy me cackle fruit.
+Starboard American Main skysail. Matey line gibbet. Chantey line cable.
--- packages/template-e2e/src/tests/blog.spec.js
+++ packages/template-e2e/src/tests/blog.spec.js
@@ -22,3 +22,62 @@ test.describe.parallel("blog index page", () => {
     await expect(page).toHaveTitle("Conquer the puddles");
   });
 });
+
+test.describe.parallel("blog page with authors in frontmatter", () => {
+  test.beforeEach(async ({ page }) => {
+    const Page = new MarkdownPage(page);
+    await Page.goto("/blog/blogtest");
+    await Page.getData();
+  });
+
+  test("displays and links to author matched by the slug", async ({ page }) => {
+    await page.locator("a", { hasText: "John the Great" }).click();
+    await expect(page).toHaveURL("/people/john-doe");
+    await expect(page).toHaveTitle("John the Great");
+  });
+
+  test("displays and links to author matched by the id", async ({ page }) => {
+    await page.locator("a", { hasText: "Jane the Great" }).click();
+    await expect(page).toHaveURL("/people/jane-doe");
+    await expect(page).toHaveTitle("Jane the Great");
+  });
+
+  test("displays and links to author matched by the name", async ({ page }) => {
+    await page.locator("a", { hasText: "Joe Bloggs" }).click();
+    await expect(page).toHaveURL("/people/joe-bloggs");
+    await expect(page).toHaveTitle("Joe Bloggs");
+  });
+
+  test("doesn't link to author page if it is a draft", async ({ page }) => {
+    await expect(page.locator("text=John Nokes")).toHaveCount(1);
+    await expect(page.locator("a", { hasText: "John Nokes" })).toHaveCount(0);
+  });
+
+  test("displays provided author name if no match found", async ({ page }) => {
+    await expect(page.locator("text=The Other Guy")).toHaveCount(1);
+  });
+
+  // test("displays author's bio on hover", async ({ page }) => {
+  // not implemented
+  // });
+
+  // test("author's page displays a list of all pages authored by them", async ({ page }) => {
+  // not implemented
+  // });
+});
+
+test.describe.parallel("blog page without authors", () => {
+  test.beforeEach(async ({ page }) => {
+    const Page = new MarkdownPage(page);
+    await Page.goto("/blogtest");
+    await Page.getData();
+  });
+
+  test("displays a default author (set in config.js) on Blogs with no frontmatter `authors` field set", async ({
+    page,
+  }) => {
+    await page.locator("a", { hasText: "John the Great" }).click();
+    await expect(page).toHaveURL("/people/john-doe");
+    await expect(page).toHaveTitle("John the Great");
+  });
+});
