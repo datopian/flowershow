c8fd411a13920a693846c40123c24abe07b25efa
olayway <ola@yway.pl>
2023-01-12 08:54:52 +0100

    [packages/core][xs]: mv default config to core

---
+++ packages/core/src/config/default.js
@@ -0,0 +1,29 @@
+export const defaultConfig = {
+  title: "Flowershow",
+  description: "",
+  showEditLink: false,
+  showToc: true,
+  author: "",
+  authorLogo: "",
+  authorUrl: "",
+  // Google analytics key e.g. G-XXXX
+  analytics: "",
+  // content source directory for markdown files
+  // DO NOT CHANGE THIS VALUE
+  // if you have your notes in another (external) directory,
+  // /content dir should be a symlink to that directory
+  content: "content",
+  avatarPlaceholder: "/_flowershow/avatarplaceholder.png",
+  contentExclude: [],
+  contentInclude: [],
+  blogDir: "blog",
+  peopleDir: "people",
+  // Theme
+  theme: {
+    default: "dark",
+    toggleIcon: "/_flowershow/theme-button.svg",
+  },
+  navLinks: [
+    // { href: '/about', name: 'About' },
+  ],
+};
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./ui";
 export * from "./utils";
+export * from "./config";
