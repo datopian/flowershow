fc514d0a78ad5cc4f68e18a9fda8b3281f794aec
olayway <ola@yway.pl>
2023-01-12 08:54:52 +0100

    [packages/core][xs]: mv default config to core

--- packages/template/config/siteConfig.js
+++ packages/template/config/siteConfig.js
@@ -1,42 +1,13 @@
-import config from "../content/config";
-
-const defaultConfig = {
-  title: "Flowershow",
-  description: "",
-  showEditLink: false,
-  showToc: true,
-  author: "",
-  authorLogo: "",
-  authorUrl: "",
-  // Google analytics key e.g. G-XXXX
-  analytics: "",
-  // content source directory for markdown files
-  // DO NOT CHANGE THIS VALUE
-  // if you have your notes in another (external) directory,
-  // /content dir should be a symlink to that directory
-  content: "content",
-  avatarPlaceholder: "/_flowershow/avatarplaceholder.png",
-  contentExclude: [],
-  contentInclude: [],
-  blogDir: "blog",
-  peopleDir: "people",
-  // Theme
-  theme: {
-    default: "dark",
-    toggleIcon: "/_flowershow/theme-button.svg",
-  },
-  navLinks: [
-    // { href: '/about', name: 'About' },
-  ],
-};
+import { defaultConfig } from "@flowershow/core";
+import userConfig from "../content/config";
 
 export const siteConfig = {
   ...defaultConfig,
-  ...config,
+  ...userConfig,
   // prevent theme object overrides for
   // values not provided in userConfig
   theme: {
     ...defaultConfig.theme,
-    ...config?.theme,
+    ...userConfig?.theme,
   },
 };
