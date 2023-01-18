f1d242340644fdd9abddbc1c99190469b8b5cd34
khalilcodes <khalilali.j@gmail.com>
2023-01-18 22:19:45 +0300

    [packages,core,#36][m]: create tooltip component and add package

--- packages/core/src/ui/Base/CustomLink.tsx
+++ packages/core/src/ui/Base/CustomLink.tsx
@@ -1,22 +1,47 @@
 import Link from "next/link.js";
+import { Tooltip } from "../Tooltip";
+
+type Config = {
+  showLinkPreviews: boolean;
+};
 
 interface Props {
   href?: string;
+  data: any;
+  usehook: any;
+  config: Config;
   [x: string]: unknown;
 }
 
-export const CustomLink: React.FC<Props> = ({ href, ...rest }) => {
+export const CustomLink: React.FC<Props> = ({
+  config,
+  data,
+  usehook,
+  ...props
+}) => {
+  const { href } = props;
   const isInternalLink = href && href.startsWith("/");
   const isAnchorLink = href && href.startsWith("#");
 
   // TODO why are we doing this?
+  // Use next link for pages within app and <a> for external links.
+  // https://nextjs.org/learn/basics/navigate-between-pages/client-side
   if (isInternalLink) {
-    return <Link href={href} {...rest} />;
+    return config.showLinkPreviews ? (
+      <Tooltip
+        {...props}
+        data={data}
+        usehook={usehook}
+        render={(tooltipTriggerProps) => <Link {...tooltipTriggerProps} />}
+      />
+    ) : (
+      <Link href={href} {...props} />
+    );
   }
 
   if (isAnchorLink) {
-    return <a href={href} {...rest} />;
+    return <a {...props} />;
   }
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />;
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
 };
