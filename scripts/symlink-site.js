9bec73a6cd992596f6282e00cf65ba6950ef09b0
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2023-01-18 13:52:21 +0300

    Allow installing as non-admin on Windows (#373)
    * [packages/cli][xs]: Fix typo in package name
    
    * [package/cli][xs]: Fix installation on Windows
    
    The 3rd parameter for `symlinkSync` is only used on Windows and ignored on other platforms.
    The autodetected one is either `file` (if the target doesn't exist) or `dir` (which requires admin privileges). The appropriate one to use is `junction`, otherwise the installation fails silently.
    
    * [package/cli][xs]: Add link type to symlink script
    
    * [packages/template][xs]: Use double quote in param
    
    Otherwise it breaks on Windows

--- scripts/symlink-site.js
+++ scripts/symlink-site.js
@@ -11,17 +11,17 @@ const excalidrawSymlink = "../packages/template/public/excalidraw";
 const componentsSymlink = "../packages/template/components/custom";
 
 if (!fs.existsSync(contentSymlink)) {
-  fs.symlinkSync("../../site/content", contentSymlink);
+  fs.symlinkSync("../../site/content", contentSymlink, 'junction');
 }
 
 if (!fs.existsSync(assetsSymlink)) {
-  fs.symlinkSync("../../../site/content/assets", assetsSymlink);
+  fs.symlinkSync("../../../site/content/assets", assetsSymlink, 'junction');
 }
 
 if (!fs.existsSync(excalidrawSymlink)) {
-  fs.symlinkSync("../../../site/content/excalidraw", excalidrawSymlink);
+  fs.symlinkSync("../../../site/content/excalidraw", excalidrawSymlink, 'junction');
 }
 
 if (!fs.existsSync(componentsSymlink)) {
-  fs.symlinkSync("../../../site/components", componentsSymlink);
+  fs.symlinkSync("../../../site/components", componentsSymlink, 'junction');
 }
