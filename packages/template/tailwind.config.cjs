4bd385ca2c325111eda3a41beb77b3a3ac2b9122
khalilcodes <khalilali.j@gmail.com>
2023-01-18 22:24:18 +0300

    [packages,template,#36][s]: add tailwind plugin and modify custom link props for tooltip component

--- packages/template/tailwind.config.cjs
+++ packages/template/tailwind.config.cjs
@@ -40,5 +40,8 @@ module.exports = {
     },
   },
   /* eslint global-require: off */
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
