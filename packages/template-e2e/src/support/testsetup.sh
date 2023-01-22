3efd7eaae3b42ccc32a7c13b7a7666867c461aa2
olayway <ola@yway.pl>
2023-01-22 18:21:52 +0100

    [packages/template-e2e][xs]: rm old, irrelevant code

--- packages/template-e2e/src/support/testsetup.sh
+++ packages/template-e2e/src/support/testsetup.sh
@@ -18,14 +18,6 @@ ln -vfns $TEMP_CONTENT_DIR $TEMP_TEMPLATE_DIR/content
 ln -vfns $TEMP_CONTENT_DIR/assets $TEMP_TEMPLATE_DIR/public/assets
 ln -vfns $TEMP_COMPONENTS_DIR $TEMP_TEMPLATE_DIR/components/custom
 
-# template package is using local versions of our remark plugins
-# this is enabled in the template's package.json with "workspace:*" used instead of a package version
-# e.g. "remark-callouts": "workspace:*"
-# we need to replace it with latest
-# TODO figure out a better way to run the tests, that would also use local versions of our remark plugins (?)
-# maybe get back to symlinking directly from the template without copying to temp dir? (better not)
-sed 's/workspace:\*/latest/' -i $TEMP_TEMPLATE_DIR/package.json
-
 cd $TEMP_TEMPLATE_DIR
 npm install
 npm run dev -- -p 3030 &
