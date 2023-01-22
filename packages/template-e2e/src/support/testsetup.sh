5963563f2c39539e7be6969fe6a4657b1a1cc211
olayway <ola@yway.pl>
2023-01-22 19:44:07 +0100

    [packages/template-e2e][s]: syml directly to template
    - symlinks fixture content directly to template (as originally, without
    copying everything to tmp directory) so that running tests is faster,
    easier to debug and more reliable as the template will local versions of our packages
    - also, creates two additional targets for manually symlinking (and
    un-symlinking) fixture content

--- packages/template-e2e/src/support/testsetup.sh
+++ packages/template-e2e/src/support/testsetup.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/sh
 
 TEMPLATE_DIR=$1
-CONTENT_DIR=$2
-COMPONENTS_DIR=$3
-E2E_TEMP_DIR=$(mktemp -d)
+TEST_CONTENT_DIR=$2
+TEST_COMPONENTS_DIR=$3
 
-cp -r $TEMPLATE_DIR $E2E_TEMP_DIR
-# TODO we don't need to copy content and components
-cp -r $CONTENT_DIR $E2E_TEMP_DIR
-cp -r $COMPONENTS_DIR $E2E_TEMP_DIR
-
-TEMP_TEMPLATE_DIR=$E2E_TEMP_DIR/$(basename $TEMPLATE_DIR)
-TEMP_CONTENT_DIR=$E2E_TEMP_DIR/$(basename $CONTENT_DIR)
-TEMP_COMPONENTS_DIR=$E2E_TEMP_DIR/$(basename $COMPONENTS_DIR)
-
-ln -vfns $TEMP_CONTENT_DIR $TEMP_TEMPLATE_DIR/content
-ln -vfns $TEMP_CONTENT_DIR/assets $TEMP_TEMPLATE_DIR/public/assets
-ln -vfns $TEMP_COMPONENTS_DIR $TEMP_TEMPLATE_DIR/components/custom
-
-cd $TEMP_TEMPLATE_DIR
-npm install
-npm run dev -- -p 3030 &
+ln -vfns $TEST_CONTENT_DIR $TEMPLATE_DIR/content
+ln -vfns $TEST_CONTENT_DIR/assets $TEMPLATE_DIR/public/assets
+ln -vfns $TEST_COMPONENTS_DIR $TEMPLATE_DIR/components/custom
