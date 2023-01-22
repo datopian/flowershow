5b64c5e641afd0476151ca9ec946eb61d36cd319
olayway <ola@yway.pl>
2023-01-22 19:44:07 +0100

    [packages/template-e2e][s]: syml directly to template
    - symlinks fixture content directly to template (as originally, without
    copying everything to tmp directory) so that running tests is faster,
    easier to debug and more reliable as the template will local versions of our packages
    - also, creates two additional targets for manually symlinking (and
    un-symlinking) fixture content

--- packages/template-e2e/playwright.config.mjs
+++ packages/template-e2e/playwright.config.mjs
@@ -6,8 +6,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Use process.env.PORT by default and fallback to port 3000
-// const PORT = process.env.PORT || 3000;
-const PORT = 3030;
+const PORT = process.env.PORT || 3000;
+// const PORT = 3030;
 
 // Set webServer.url and use.baseURL with the location of the WebServer respecting the correct set port
 const baseURL = `http://localhost:${PORT}`;
@@ -15,7 +15,7 @@ const baseURL = `http://localhost:${PORT}`;
 // Reference: https://playwright.dev/docs/test-configuration
 const config = {
   // Timeout per test
-  timeout: 60 * 1000,
+  timeout: 30 * 1000,
   // Test directory
   testDir: path.join(__dirname, "src/tests"),
   // If a test fails, retry it additional 2 times
