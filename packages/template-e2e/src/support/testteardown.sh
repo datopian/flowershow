d7580c718f7844242bebd13a05ae9741ca82db6d
olayway <ola@yway.pl>
2023-01-26 16:33:56 +0100

    [packages/template-e2e][xs]: rm test that requires postbuild

--- packages/template-e2e/src/support/testteardown.sh
+++ packages/template-e2e/src/support/testteardown.sh
@@ -7,6 +7,3 @@ COMPONENTS_DIR=$3
 ln -vfns $CONTENT_DIR $TEMPLATE_DIR/content
 ln -vfns $CONTENT_DIR/assets $TEMPLATE_DIR/public/assets
 ln -vfns $COMPONENTS_DIR $TEMPLATE_DIR/components/custom
-
-# recreate public/search.json file
-npm run postbuild --prefix $TEMPLATE_DIR
