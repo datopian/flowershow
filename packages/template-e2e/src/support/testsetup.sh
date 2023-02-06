a87f84c59923002221566c666b4b90ce5d9af3af
olayway <ola@yway.pl>
2023-02-06 23:04:13 +0100

    fix testsetup links

--- packages/template-e2e/src/support/testsetup.sh
+++ packages/template-e2e/src/support/testsetup.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-TEMPLATE_DIR=$1
-TEST_CONTENT_DIR=$2
-TEST_COMPONENTS_DIR=$3
-
-ln -vfns $TEST_CONTENT_DIR $TEMPLATE_DIR/content
-ln -vfns $TEST_CONTENT_DIR/assets $TEMPLATE_DIR/public/assets
-ln -vfns $TEST_COMPONENTS_DIR $TEMPLATE_DIR/components/custom
+ln -vfns ../../site/content content
+ln -vfns ../../../site/content/assets public/assets
+ln -vfns ../../../site/components components/custom
