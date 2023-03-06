1a44bf5397ca4f1b019194a7de37c2edb661b13e
philippedupreez <philippedupreez79@gmail.com>
2023-03-06 15:02:31 +0200

    [core/src/ui/Blog][s]: Make date the default field in frontmatter

--- packages/core/src/ui/BlogLayout/BlogLayout.tsx
+++ packages/core/src/ui/BlogLayout/BlogLayout.tsx
@@ -6,16 +6,16 @@ import { Avatar } from "../Avatar";
 type Props = any;
 
 export const BlogLayout: React.FC<Props> = ({ children, ...frontMatter }) => {
-  const { title, created, authorsDetails } = frontMatter;
+  const { title, date, authorsDetails } = frontMatter;
 
   return (
     <article className="docs prose prose-a:text-primary dark:prose-a:text-primary-dark prose-strong:text-primary dark:prose-strong:text-primary-dark prose-code:text-primary dark:prose-code:text-primary-dark prose-headings:text-primary dark:prose-headings:text-primary-dark prose text-primary dark:text-primary-dark prose-headings:font-headings dark:prose-invert prose-a:break-words mx-auto p-6">
       <header>
         <div className="mb-4 flex-col items-center">
           {title && <h1 className="flex justify-center">{title}</h1>}
-          {created && (
+          {date && (
             <p className="text-sm text-zinc-400 dark:text-zinc-500 flex justify-center">
-              <time dateTime={created}>{formatDate(created)}</time>
+              <time dateTime={date}>{formatDate(date)}</time>
             </p>
           )}
           {authorsDetails && (
