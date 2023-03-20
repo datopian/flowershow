ec09da1bb712c7306f503934de1c4412e05ebdd3
olayway <ola@yway.pl>
2023-03-20 19:01:38 +0100

    [packages/template-e2e/fixtures]: getter adjust

--- packages/template-e2e/src/fixtures/content/getters/blogs.js
+++ packages/template-e2e/src/fixtures/content/getters/blogs.js
@@ -1,5 +1,5 @@
 import { allBlogs } from "contentlayer/generated";
 
 export default function getBlogs() {
-  return allBlogs.sort((a, b) => new Date(b.created) - new Date(a.created));
+  return allBlogs.sort((a, b) => new Date(b.date) - new Date(a.date));
 }
