d357c40ae115a508144c5700a28c90fa4634cc88
olayway <ola@yway.pl>
2023-03-29 00:31:44 +0200

    test all config options

--- packages/remark-wiki-link/test/index.spec.ts
+++ packages/remark-wiki-link/test/index.spec.ts
@@ -10,9 +10,14 @@ describe("remark-wiki-link", () => {
     const processor = unified()
       .use(markdown)
       .use(wikiLinkPlugin, {
-        permalinks: ["/some/folder/real-page"],
         aliasDivider: ":",
         pathFormat: "obsidian-short",
+        permalinks: ["/some/folder/123/real-page"],
+        pageResolver: (pageName: string) => [
+          `123/${pageName.replace(/ /g, "-").toLowerCase()}`,
+        ],
+        wikiLinkClassName: "my-wiki-link-class",
+        hrefTemplate: (permalink: string) => `https://my-site.com${permalink}`,
       });
 
     let ast = processor.parse("[[Real Page#With Heading:Page Alias]]");
@@ -20,12 +25,14 @@ describe("remark-wiki-link", () => {
 
     visit(ast, "wikiLink", (node: Node) => {
       expect(node.data.exists).toEqual(true);
-      expect(node.data.permalink).toEqual("/some/folder/real-page");
+      expect(node.data.permalink).toEqual("/some/folder/123/real-page");
       expect(node.data.alias).toEqual("Page Alias");
       expect(node.data.hName).toEqual("a");
-      expect((node.data.hProperties as any).className).toEqual("internal");
+      expect((node.data.hProperties as any).className).toEqual(
+        "my-wiki-link-class"
+      );
       expect((node.data.hProperties as any).href).toEqual(
-        "/some/folder/real-page#with-heading"
+        "https://my-site.com/some/folder/123/real-page#with-heading"
       );
       expect((node.data.hChildren as any)[0].value).toEqual("Page Alias");
     });
