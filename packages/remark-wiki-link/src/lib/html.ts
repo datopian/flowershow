0cd86dd2850d8b08037bd9cc409dc15358f8d4e4
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-03-29 16:41:17 +0200

    Remark wiki link refactor (#467)
    ## Changes
    - `@flowershow/remark-wiki-link`
      - general code refactoring: tidying up, simplifying, using better naming etc.
      - disable passing markdown folder in plugin config -> export `getPermalinks` function, so that users (e.g. our template) can run it on their side and then use a standard config option `permalinks` to explicitly pass the permalinks list
      - extend tests coverage
      - refactor to TS
      - replace `mocha` with `jest`
    - `@flowershow/template`
      - import `getPermalinks` from `@flowershow/remark-wiki-link`, generate the permalinks list from the content folder on the template side (in `contentlayer.config.ts`), and pass it explicitly to the plugin instead of passing the path to markdown folder to it
    
    ## Notes
    I think there should be a way of using `micromark` extensions only - our `html` and `syntax` -  but maybe I'm missing sth (still not quite getting all the different elements of the system and ecosystem...) and we actually need to use `syntax` extension for `micromark` combined with `fromMarkdown` and `toMarkdown` for `mdast-util-from-markdown`. I actually started by writing `syntax` and `html` and both of them used in `micromark` do their job, i.e. they correctly convert md to html (see tests in `remark-wiki-link/tests/micromarkExtensionWikiLink.spec.ts` - mirror tests of `fromMarkdown`). But I couldn't make it work without using `fromMarkdown` - **which is almost identical to `html`** - and `toMarkdown`.
    For reference see also `remark-wiki-link/src/lib/remarkWikiLink.ts` file.

---
+++ packages/remark-wiki-link/src/lib/html.ts
@@ -0,0 +1,142 @@
+import { isSupportedFileFormat } from "./isSupportedFileFormat";
+
+export interface HtmlOptions {
+  pathFormat?:
+    | "raw" // default; use for regular relative or absolute paths
+    | "obsidian-absolute" // use for Obsidian-style absolute paths, i.e. with no leading slash
+    | "obsidian-short"; // use for Obsidian-style shortened paths
+  permalinks?: string[];
+  pageResolver?: (name: string) => string[];
+  newClassName?: string;
+  wikiLinkClassName?: string;
+  hrefTemplate?: (permalink: string) => string;
+}
+
+// Micromark HtmlExtension
+// https://github.com/micromark/micromark#htmlextension
+function html(opts: HtmlOptions = {}) {
+  const pathFormat = opts.pathFormat || "raw";
+  const permalinks = opts.permalinks || [];
+  const defaultPageResolver = (name: string, isEmbed: boolean) => {
+    const page = isEmbed
+      ? name
+      : name
+          .replace(/ /g, "-")
+          .replace(/\/index$/, "")
+          .toLowerCase();
+    if (pathFormat === "obsidian-absolute") {
+      return [`/${page}`];
+    }
+    return [page];
+  };
+  const pageResolver = opts.pageResolver || defaultPageResolver;
+  const newClassName = opts.newClassName || "new";
+  const wikiLinkClassName = opts.wikiLinkClassName || "internal";
+  const defaultHrefTemplate = (permalink: string) => permalink;
+  const hrefTemplate = opts.hrefTemplate || defaultHrefTemplate;
+
+  function top(stack) {
+    return stack[stack.length - 1];
+  }
+
+  function enterWikiLink() {
+    let stack = this.getData("wikiLinkStack");
+    if (!stack) this.setData("wikiLinkStack", (stack = []));
+
+    stack.push({});
+  }
+
+  function exitWikiLinkTarget(token) {
+    const target = this.sliceSerialize(token);
+    const current = top(this.getData("wikiLinkStack"));
+    current.target = target;
+  }
+
+  function exitWikiLinkAlias(token) {
+    const alias = this.sliceSerialize(token);
+    const current = top(this.getData("wikiLinkStack"));
+    current.alias = alias;
+  }
+
+  function exitWikiLink(token) {
+    const wikiLink = this.getData("wikiLinkStack").pop();
+    const { target, alias } = wikiLink;
+    const isEmbed = token.isType === "embed";
+
+    const resolveShortenedPaths = pathFormat === "obsidian-short";
+    const pagePermalinks = pageResolver(target, isEmbed);
+
+    // eslint-disable-next-line no-useless-escape
+    const pathWithOptionalHeadingPattern = /([a-z0-9\.\/_-]*)(#.*)?/;
+    let targetHeading = "";
+
+    const matchingPermalink = permalinks.find((e) => {
+      return pagePermalinks.find((p) => {
+        const [, pagePath, heading] = p.match(pathWithOptionalHeadingPattern);
+        if (resolveShortenedPaths) {
+          if (e === pagePath || e.endsWith(pagePath)) {
+            targetHeading = heading ?? "";
+            return true;
+          }
+          return false;
+        } else {
+          if (e === pagePath) {
+            targetHeading = heading ?? "";
+            return true;
+          }
+          return false;
+        }
+      });
+    });
+
+    const permalink = matchingPermalink || pagePermalinks[0];
+
+    // remove leading # if the target is a heading on the same page
+    const displayName = alias || target.replace(/^#/, "");
+
+    let classNames = wikiLinkClassName;
+    if (!matchingPermalink) {
+      classNames += " " + newClassName;
+    }
+
+    if (isEmbed) {
+      const [isSupportedFormat, format] = isSupportedFileFormat(target);
+      if (!isSupportedFormat) {
+        this.raw(`![[${target}]]`);
+      } else if (format === "pdf") {
+        this.tag(
+          `<iframe width="100%" src="${hrefTemplate(
+            permalink
+          )}#toolbar=0" class="${classNames}" />`
+        );
+      } else {
+        this.tag(
+          `<img src="${hrefTemplate(
+            permalink
+          )}" alt="${displayName}" class="${classNames}" />`
+        );
+      }
+    } else {
+      this.tag(
+        `<a href="${hrefTemplate(
+          permalink + targetHeading
+        )}" class="${classNames}">`
+      );
+      this.raw(displayName);
+      this.tag("</a>");
+    }
+  }
+
+  return {
+    enter: {
+      wikiLink: enterWikiLink,
+    },
+    exit: {
+      wikiLinkTarget: exitWikiLinkTarget,
+      wikiLinkAlias: exitWikiLinkAlias,
+      wikiLink: exitWikiLink,
+    },
+  };
+}
+
+export { html };
