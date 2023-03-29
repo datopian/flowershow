fa647167003cd626f5d97b1262a7ca2392877a8c
olayway <ola@yway.pl>
2023-03-29 02:04:39 +0200

    async makeSource

--- packages/template/contentlayer.config.ts
+++ packages/template/contentlayer.config.ts
@@ -18,7 +18,7 @@ import smartypants from "remark-smartypants";
 import remarkToc from "remark-toc";
 import callouts from "@flowershow/remark-callouts";
 import remarkEmbed from "@flowershow/remark-embed";
-import wikiLinkPlugin from "@flowershow/remark-wiki-link";
+import { remarkWikiLink, getPermalinks } from "@flowershow/remark-wiki-link";
 import stripMarkdown, { Options } from "strip-markdown";
 
 import { siteConfig } from "./config/siteConfig";
@@ -192,75 +192,79 @@ const contentLayerExcludeDefaults = [
 ];
 
 /* eslint import/no-default-export: off */
-export default makeSource({
-  contentDirPath: siteConfig.content,
-  contentDirExclude: contentLayerExcludeDefaults.concat([
-    ".flowershow",
-    ".obsidian",
-    ...siteConfig.contentExclude,
-  ]),
-  contentDirInclude: siteConfig.contentInclude,
-  documentTypes: [Blog, Person, Page],
-  mdx: {
-    esbuildOptions: (opts) => {
-      opts.tsconfig = `${process.env.PWD}/tsconfig.mdx.json`;
-      return opts;
+export default makeSource(async () => {
+  const permalinks = await getPermalinks(siteConfig.content);
+
+  return {
+    contentDirPath: siteConfig.content,
+    contentDirExclude: contentLayerExcludeDefaults.concat([
+      ".flowershow",
+      ".obsidian",
+      ...siteConfig.contentExclude,
+    ]),
+    contentDirInclude: siteConfig.contentInclude,
+    documentTypes: [Blog, Person, Page],
+    mdx: {
+      esbuildOptions: (opts) => {
+        opts.tsconfig = `${process.env.PWD}/tsconfig.mdx.json`;
+        return opts;
+      },
+      cwd: process.cwd(),
+      remarkPlugins: [
+        remarkEmbed,
+        remarkGfm,
+        [smartypants, { quotes: false, dashes: "oldschool" }],
+        remarkMath,
+        callouts,
+        [remarkWikiLink, { permalinks, pathFormat: "obsidian-short" }],
+        [
+          remarkToc,
+          {
+            heading: "Table of contents",
+            // maxDepth: "3",
+            tight: true,
+          },
+        ],
+        [mdxMermaid, {}],
+      ],
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: { className: "heading-link" },
+            test(element) {
+              return (
+                ["h2", "h3", "h4", "h5", "h6"].includes(element.tagName) &&
+                element.properties?.id !== "table-of-contents" &&
+                element.properties?.className !== "blockquote-heading"
+              );
+            },
+            content() {
+              return [
+                h(
+                  "svg",
+                  {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    fill: "#ab2b65",
+                    viewBox: "0 0 20 20",
+                    className: "w-5 h-5",
+                  },
+                  [
+                    h("path", {
+                      fillRule: "evenodd",
+                      clipRule: "evenodd",
+                      d: "M9.493 2.853a.75.75 0 00-1.486-.205L7.545 6H4.198a.75.75 0 000 1.5h3.14l-.69 5H3.302a.75.75 0 000 1.5h3.14l-.435 3.148a.75.75 0 001.486.205L7.955 14h2.986l-.434 3.148a.75.75 0 001.486.205L12.456 14h3.346a.75.75 0 000-1.5h-3.14l.69-5h3.346a.75.75 0 000-1.5h-3.14l.435-3.147a.75.75 0 00-1.486-.205L12.045 6H9.059l.434-3.147zM8.852 7.5l-.69 5h2.986l.69-5H8.852z",
+                    }),
+                  ]
+                ),
+              ];
+            },
+          },
+        ],
+        rehypeMathjax,
+        [rehypePrismPlus, { ignoreMissing: true }],
+      ],
     },
-    cwd: process.cwd(),
-    remarkPlugins: [
-      remarkEmbed,
-      remarkGfm,
-      [smartypants, { quotes: false, dashes: "oldschool" }],
-      remarkMath,
-      callouts,
-      [wikiLinkPlugin, { markdownFolder: siteConfig.content }],
-      [
-        remarkToc,
-        {
-          heading: "Table of contents",
-          // maxDepth: "3",
-          tight: true,
-        },
-      ],
-      [mdxMermaid, {}],
-    ],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          properties: { className: "heading-link" },
-          test(element) {
-            return (
-              ["h2", "h3", "h4", "h5", "h6"].includes(element.tagName) &&
-              element.properties?.id !== "table-of-contents" &&
-              element.properties?.className !== "blockquote-heading"
-            );
-          },
-          content() {
-            return [
-              h(
-                "svg",
-                {
-                  xmlns: "http://www.w3.org/2000/svg",
-                  fill: "#ab2b65",
-                  viewBox: "0 0 20 20",
-                  className: "w-5 h-5",
-                },
-                [
-                  h("path", {
-                    fillRule: "evenodd",
-                    clipRule: "evenodd",
-                    d: "M9.493 2.853a.75.75 0 00-1.486-.205L7.545 6H4.198a.75.75 0 000 1.5h3.14l-.69 5H3.302a.75.75 0 000 1.5h3.14l-.435 3.148a.75.75 0 001.486.205L7.955 14h2.986l-.434 3.148a.75.75 0 001.486.205L12.456 14h3.346a.75.75 0 000-1.5h-3.14l.69-5h3.346a.75.75 0 000-1.5h-3.14l.435-3.147a.75.75 0 00-1.486-.205L12.045 6H9.059l.434-3.147zM8.852 7.5l-.69 5h2.986l.69-5H8.852z",
-                  }),
-                ]
-              ),
-            ];
-          },
-        },
-      ],
-      rehypeMathjax,
-      [rehypePrismPlus, { ignoreMissing: true }],
-    ],
-  },
+  };
 });
