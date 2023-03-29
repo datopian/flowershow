71110e23a135cde486fd5090a428e11be6d2b335
olayway <ola@yway.pl>
2023-03-29 19:43:07 +0200

    [packages/remark-wiki-link][s]: fix links to index pages

--- packages/remark-wiki-link/test/remarkWikiLink.spec.ts
+++ packages/remark-wiki-link/test/remarkWikiLink.spec.ts
@@ -15,17 +15,17 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        expect(node.data.exists).toEqual(false);
-        expect(node.data.permalink).toEqual("../some/folder/wiki-link");
-        expect(node.data.alias).toEqual(null);
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual(
+        expect(node.data?.exists).toEqual(false);
+        expect(node.data?.permalink).toEqual("../some/folder/wiki-link");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual(
           "internal new"
         );
-        expect((node.data.hProperties as any).href).toEqual(
+        expect((node.data?.hProperties as any).href).toEqual(
           "../some/folder/wiki-link"
         );
-        expect((node.data.hChildren as any)[0].value).toEqual(
+        expect((node.data?.hChildren as any)[0].value).toEqual(
           "../some/folder/Wiki Link"
         );
       });
@@ -40,17 +40,17 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        expect(node.data.exists).toEqual(false);
-        expect(node.data.permalink).toEqual("/some/folder/wiki-link");
-        expect(node.data.alias).toEqual(null);
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual(
+        expect(node.data?.exists).toEqual(false);
+        expect(node.data?.permalink).toEqual("/some/folder/wiki-link");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual(
           "internal new"
         );
-        expect((node.data.hProperties as any).href).toEqual(
+        expect((node.data?.hProperties as any).href).toEqual(
           "/some/folder/wiki-link"
         );
-        expect((node.data.hChildren as any)[0].value).toEqual(
+        expect((node.data?.hChildren as any)[0].value).toEqual(
           "some/folder/Wiki Link"
         );
       });
@@ -68,15 +68,15 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        expect(node.data.exists).toEqual(true);
-        expect(node.data.permalink).toEqual("/some/folder/wiki-link");
-        expect(node.data.alias).toEqual(null);
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual("internal");
-        expect((node.data.hProperties as any).href).toEqual(
+        expect(node.data?.exists).toEqual(true);
+        expect(node.data?.permalink).toEqual("/some/folder/wiki-link");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual("internal");
+        expect((node.data?.hProperties as any).href).toEqual(
           "/some/folder/wiki-link"
         );
-        expect((node.data.hChildren as any)[0].value).toEqual("Wiki Link");
+        expect((node.data?.hChildren as any)[0].value).toEqual("Wiki Link");
       });
     });
   });
@@ -93,13 +93,13 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        expect(node.data.exists).toEqual(true);
-        expect(node.data.permalink).toEqual("wiki-link");
-        expect(node.data.alias).toEqual(null);
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual("internal");
-        expect((node.data.hProperties as any).href).toEqual("wiki-link");
-        expect((node.data.hChildren as any)[0].value).toEqual("Wiki Link");
+        expect(node.data?.exists).toEqual(true);
+        expect(node.data?.permalink).toEqual("wiki-link");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual("internal");
+        expect((node.data?.hProperties as any).href).toEqual("wiki-link");
+        expect((node.data?.hChildren as any)[0].value).toEqual("Wiki Link");
       });
     });
 
@@ -114,15 +114,15 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        expect(node.data.exists).toEqual(false);
-        expect(node.data.permalink).toEqual("wiki-link");
-        expect(node.data.alias).toEqual(null);
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual(
+        expect(node.data?.exists).toEqual(false);
+        expect(node.data?.permalink).toEqual("wiki-link");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual(
           "internal new"
         );
-        expect((node.data.hProperties as any).href).toEqual("wiki-link");
-        expect((node.data.hChildren as any)[0].value).toEqual("Wiki Link");
+        expect((node.data?.hProperties as any).href).toEqual("wiki-link");
+        expect((node.data?.hChildren as any)[0].value).toEqual("Wiki Link");
       });
     });
 
@@ -138,15 +138,15 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        expect(node.data.exists).toEqual(true);
-        expect(node.data.permalink).toEqual("/some/folder/wiki-link");
-        expect(node.data.alias).toEqual(null);
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual("internal");
-        expect((node.data.hProperties as any).href).toEqual(
+        expect(node.data?.exists).toEqual(true);
+        expect(node.data?.permalink).toEqual("/some/folder/wiki-link");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual("internal");
+        expect((node.data?.hProperties as any).href).toEqual(
           "/some/folder/wiki-link"
         );
-        expect((node.data.hChildren as any)[0].value).toEqual("Wiki Link");
+        expect((node.data?.hChildren as any)[0].value).toEqual("Wiki Link");
       });
     });
   });
@@ -159,17 +159,17 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        expect(node.data.exists).toEqual(false);
-        expect(node.data.permalink).toEqual("wiki-link#some-heading"); // TODO should this be "wiki-link" only?
-        expect(node.data.alias).toEqual(null);
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual(
+        expect(node.data?.exists).toEqual(false);
+        expect(node.data?.permalink).toEqual("wiki-link#some-heading"); // TODO should this be "wiki-link" only?
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual(
           "internal new"
         );
-        expect((node.data.hProperties as any).href).toEqual(
+        expect((node.data?.hProperties as any).href).toEqual(
           "wiki-link#some-heading"
         );
-        expect((node.data.hChildren as any)[0].value).toEqual(
+        expect((node.data?.hChildren as any)[0].value).toEqual(
           "Wiki Link#Some Heading"
         );
       });
@@ -182,17 +182,17 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        expect(node.data.exists).toEqual(false);
-        expect(node.data.permalink).toEqual("wiki-link#some-heading"); // TODO should this be "wiki-link" only?
-        expect(node.data.alias).toEqual("Alias");
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual(
+        expect(node.data?.exists).toEqual(false);
+        expect(node.data?.permalink).toEqual("wiki-link#some-heading"); // TODO should this be "wiki-link" only?
+        expect(node.data?.alias).toEqual("Alias");
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual(
           "internal new"
         );
-        expect((node.data.hProperties as any).href).toEqual(
+        expect((node.data?.hProperties as any).href).toEqual(
           "wiki-link#some-heading"
         );
-        expect((node.data.hChildren as any)[0].value).toEqual("Alias");
+        expect((node.data?.hChildren as any)[0].value).toEqual("Alias");
       });
     });
 
@@ -203,15 +203,15 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "wikiLink", (node: Node) => {
-        // expect(node.data.exists).toEqual(false); // TODO: should this be true?
-        // expect(node.data.permalink).toEqual(""); // TODO: should this be null?
-        expect(node.data.alias).toEqual(null);
-        expect(node.data.hName).toEqual("a");
-        expect((node.data.hProperties as any).className).toEqual(
+        // expect(node.data?.exists).toEqual(false); // TODO: should this be true?
+        // expect(node.data?.permalink).toEqual(""); // TODO: should this be null?
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual(
           "internal new"
         );
-        expect((node.data.hProperties as any).href).toEqual("#some-heading");
-        expect((node.data.hChildren as any)[0].value).toEqual("Some Heading");
+        expect((node.data?.hProperties as any).href).toEqual("#some-heading");
+        expect((node.data?.hChildren as any)[0].value).toEqual("Some Heading");
       });
     });
   });
@@ -224,9 +224,9 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "image", (node: Node) => {
-        expect(node.data.permalink).toEqual("../some/folder/My Image.png");
-        expect(node.data.hName).toEqual("img");
-        expect((node.data.hProperties as any).src).toEqual(
+        expect(node.data?.permalink).toEqual("../some/folder/My Image.png");
+        expect(node.data?.hName).toEqual("img");
+        expect((node.data?.hProperties as any).src).toEqual(
           "../some/folder/My Image.png"
         );
       });
@@ -239,9 +239,9 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "image", (node: Node) => {
-        expect(node.data.permalink).toEqual("../some/folder/My Image.xyz");
-        expect(node.data.hName).toEqual("p");
-        expect((node.data.hChildren as any)[0].value).toEqual(
+        expect(node.data?.permalink).toEqual("../some/folder/My Image.xyz");
+        expect(node.data?.hName).toEqual("p");
+        expect((node.data?.hChildren as any)[0].value).toEqual(
           "![[../some/folder/My Image.xyz]]"
         );
       });
@@ -254,12 +254,12 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "image", (node: Node) => {
-        expect(node.data.permalink).toEqual("../some/folder/My Image.png");
-        expect(node.data.hName).toEqual("img");
-        expect((node.data.hProperties as any).src).toEqual(
+        expect(node.data?.permalink).toEqual("../some/folder/My Image.png");
+        expect(node.data?.hName).toEqual("img");
+        expect((node.data?.hProperties as any).src).toEqual(
           "../some/folder/My Image.png"
         );
-        expect((node.data.hProperties as any).alt).toEqual("Alt Text");
+        expect((node.data?.hProperties as any).alt).toEqual("Alt Text");
       });
     });
 
@@ -270,9 +270,9 @@ describe("remark-wiki-link", () => {
       ast = processor.runSync(ast);
 
       visit(ast, "image", (node: Node) => {
-        expect(node.data.permalink).toEqual("../some/folder/My Document.pdf");
-        expect(node.data.hName).toEqual("iframe");
-        expect((node.data.hProperties as any).src).toEqual(
+        expect(node.data?.permalink).toEqual("../some/folder/My Document.pdf");
+        expect(node.data?.hName).toEqual("iframe");
+        expect((node.data?.hProperties as any).src).toEqual(
           "../some/folder/My Document.pdf"
         );
       });
@@ -335,17 +335,17 @@ describe("remark-wiki-link", () => {
     ast = processor.runSync(ast);
 
     visit(ast, "wikiLink", (node: Node) => {
-      expect(node.data.exists).toEqual(true);
-      expect(node.data.permalink).toEqual("/some/folder/123/real-page");
-      expect(node.data.alias).toEqual("Page Alias");
-      expect(node.data.hName).toEqual("a");
-      expect((node.data.hProperties as any).className).toEqual(
+      expect(node.data?.exists).toEqual(true);
+      expect(node.data?.permalink).toEqual("/some/folder/123/real-page");
+      expect(node.data?.alias).toEqual("Page Alias");
+      expect(node.data?.hName).toEqual("a");
+      expect((node.data?.hProperties as any).className).toEqual(
         "my-wiki-link-class"
       );
-      expect((node.data.hProperties as any).href).toEqual(
+      expect((node.data?.hProperties as any).href).toEqual(
         "https://my-site.com/some/folder/123/real-page#some-heading"
       );
-      expect((node.data.hChildren as any)[0].value).toEqual("Page Alias");
+      expect((node.data?.hChildren as any)[0].value).toEqual("Page Alias");
     });
   });
 
@@ -356,15 +356,97 @@ describe("remark-wiki-link", () => {
     ast = processor.runSync(ast);
 
     visit(ast, "wikiLink", (node: Node) => {
-      expect(node.data.exists).toEqual(false);
-      expect(node.data.permalink).toEqual("/some/folder");
-      expect(node.data.alias).toEqual(null);
-      expect(node.data.hName).toEqual("a");
-      expect((node.data.hProperties as any).className).toEqual("internal new");
-      expect((node.data.hProperties as any).href).toEqual("/some/folder");
-      expect((node.data.hChildren as any)[0].value).toEqual(
+      expect(node.data?.exists).toEqual(false);
+      expect(node.data?.permalink).toEqual("/some/folder");
+      expect(node.data?.alias).toEqual(null);
+      expect(node.data?.hName).toEqual("a");
+      expect((node.data?.hProperties as any).className).toEqual("internal new");
+      expect((node.data?.hProperties as any).href).toEqual("/some/folder");
+      expect((node.data?.hChildren as any)[0].value).toEqual(
         "/some/folder/index"
       );
+    });
+  });
+
+  describe("other", () => {
+    test("parses a wiki link to some index page in a folder with no matching permalink", () => {
+      const processor = unified().use(markdown).use(wikiLinkPlugin);
+
+      let ast = processor.parse("[[/some/folder/index]]");
+      ast = processor.runSync(ast);
+
+      visit(ast, "wikiLink", (node: Node) => {
+        expect(node.data?.exists).toEqual(false);
+        expect(node.data?.permalink).toEqual("/some/folder");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual(
+          "internal new"
+        );
+        expect((node.data?.hProperties as any).href).toEqual("/some/folder");
+        expect((node.data?.hChildren as any)[0].value).toEqual(
+          "/some/folder/index"
+        );
+      });
+    });
+
+    test("parses a wiki link to some index page in a folder with a matching permalink", () => {
+      const processor = unified()
+        .use(markdown)
+        .use(wikiLinkPlugin, { permalinks: ["/some/folder"] });
+
+      let ast = processor.parse("[[/some/folder/index]]");
+      ast = processor.runSync(ast);
+
+      visit(ast, "wikiLink", (node: Node) => {
+        expect(node.data?.exists).toEqual(true);
+        expect(node.data?.permalink).toEqual("/some/folder");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual("internal");
+        expect((node.data?.hProperties as any).href).toEqual("/some/folder");
+        expect((node.data?.hChildren as any)[0].value).toEqual(
+          "/some/folder/index"
+        );
+      });
+    });
+
+    test("parses a wiki link to home index page with no matching permalink", () => {
+      const processor = unified().use(markdown).use(wikiLinkPlugin);
+
+      let ast = processor.parse("[[/index]]");
+      ast = processor.runSync(ast);
+
+      visit(ast, "wikiLink", (node: Node) => {
+        expect(node.data?.exists).toEqual(false);
+        expect(node.data?.permalink).toEqual("/");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual(
+          "internal new"
+        );
+        expect((node.data?.hProperties as any).href).toEqual("/");
+        expect((node.data?.hChildren as any)[0].value).toEqual("/index");
+      });
+    });
+
+    test("parses a wiki link to home index page with a matching permalink", () => {
+      const processor = unified()
+        .use(markdown)
+        .use(wikiLinkPlugin, { permalinks: ["/"] });
+
+      let ast = processor.parse("[[/index]]");
+      ast = processor.runSync(ast);
+
+      visit(ast, "wikiLink", (node: Node) => {
+        expect(node.data?.exists).toEqual(true);
+        expect(node.data?.permalink).toEqual("/");
+        expect(node.data?.alias).toEqual(null);
+        expect(node.data?.hName).toEqual("a");
+        expect((node.data?.hProperties as any).className).toEqual("internal");
+        expect((node.data?.hProperties as any).href).toEqual("/");
+        expect((node.data?.hChildren as any)[0].value).toEqual("/index");
+      });
     });
   });
 });
