cf95fba50500a91682c58619ec2de99dbc249016
olayway <ola@yway.pl>
2023-03-29 19:45:04 +0200

    [packages/core][xs]: fix logic for testing if a link is internal

--- packages/core/src/ui/Base/CustomLink.tsx
+++ packages/core/src/ui/Base/CustomLink.tsx
@@ -2,6 +2,7 @@ import Link from "next/link.js";
 import { Tooltip } from "../Tooltip";
 import TwitterEmbed from "./TwitterEmbed";
 
+// TODO it's a mess, move twitter embeds support to remark-embed
 const TWITTER_REGEX =
   /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)/;
 
@@ -10,6 +11,8 @@ interface Props {
   data: any;
   usehook: any;
   preview: boolean;
+  children: React.ReactNode;
+  className?: string;
   [x: string]: unknown;
 }
 
@@ -17,11 +20,14 @@ export const CustomLink: React.FC<Props> = ({
   data,
   usehook,
   preview,
+  children,
   ...props
 }) => {
-  const { href } = props; // keep href in props to render tooltip content
-  const isInternalLink = href && href.startsWith("/");
-  const isAnchorLink = href && href.startsWith("#");
+  const { href, className } = props; // keep href in props to render tooltip content
+  const isInternalLink = className?.includes("internal");
+  // eslint-disable-next-line no-useless-escape
+  const isHeadingLink = /#[\w\-]* $/.test(href);
+  const isTwitterLink = TWITTER_REGEX.test(href);
 
   // Use next link for pages within app and <a> for external links.
   // https://nextjs.org/learn/basics/navigate-between-pages/client-side
@@ -29,22 +35,27 @@ export const CustomLink: React.FC<Props> = ({
     return preview ? (
       <Tooltip
         {...props}
-        data={data}
+        data={data} // TODO again, why do we pass all documents here?!
         usehook={usehook}
         render={(tooltipTriggerProps) => <Link {...tooltipTriggerProps} />}
       />
     ) : (
-      <Link {...props} />
+      <Link {...props}>{children}</Link>
     );
   }
 
-  if (isAnchorLink) {
-    return <a {...props} />;
+  if (isHeadingLink) {
+    return <Link {...props}>{children}</Link>;
   }
 
-  if (TWITTER_REGEX.test(href)) {
+  if (isTwitterLink) {
     return <TwitterEmbed url={href} {...props} />;
   }
 
-  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+  return (
+    <a target="_blank" rel="noopener noreferrer" {...props}>
+      {" "}
+      {children}
+    </a>
+  );
 };
