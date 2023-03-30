7e1e365f7b97951a9f9745260dd4d43aff3e1ec2
olayway <ola@yway.pl>
2023-03-30 02:32:10 +0200

    more tests

--- packages/remark-wiki-link/src/utils/extractLinks.ts
+++ packages/remark-wiki-link/src/utils/extractLinks.ts
@@ -3,24 +3,36 @@ import markdown from "remark-parse";
 import { unified } from "unified";
 import { selectAll } from "unist-util-select";
 // import { Node } from "unist";
+// import gfm from "remark-gfm";
 
 import remarkWikiLink from "../lib/remarkWikiLink";
 
 // TODO pass file path or slug?
 const extractLinks = (text: string, filePath?: string) => {
-  const processor = unified().use(markdown).use(remarkWikiLink);
+  const processor = unified()
+    .use(markdown)
+    // .use(gfm)
+    .use(remarkWikiLink);
 
   const ast = processor.parse(text);
 
-  const wikiLinks = selectAll("wikiLink", ast).map(
-    (node: any) => node.data?.hProperties?.href
-  );
+  // WikiLinks
+  const wikiLinks = selectAll("wikiLink", ast).map((node: any) => {
+    // href for links, src for embedded images/pdfs
+    const { href, src } = node.data?.hProperties || {};
+    return href ?? src;
+  });
 
+  // CommonMark links
   const links = selectAll("link", ast)
     .map((node: any) => node.url)
     .filter((url: string) => !url.startsWith("http"));
 
-  const allLinks = wikiLinks.concat(links);
+  const images = selectAll("image", ast)
+    .map((node: any) => node.src)
+    .filter((url: string) => !url.startsWith("http"));
+
+  const allLinks = wikiLinks.concat(links, images);
   const uniqueLinks = [...new Set(allLinks)];
 
   // convert relative links to absolute links
--- packages/remark-wiki-link/test/extractLinks.spec.ts
+++ packages/remark-wiki-link/test/extractLinks.spec.ts
@@ -21,6 +21,22 @@ describe("extractLinks", () => {
     });
   });
 
+  test("should extract embed type wikiLinks", () => {
+    const source = "![[My File.png]]]]";
+    const expectedLinks = ["My File.png"];
+    const links = extractLinks(source);
+    expect(links[0]).toBe(expectedLinks[0]);
+  });
+
+  test("should extract embed type CommonMark links", () => {
+    const source = "![](My File.png)";
+    const expectedLinks = ["My File.png"];
+    const links = extractLinks(source);
+    expect(links[0]).toBe(expectedLinks[0]);
+  });
+
+  // TODO test for links with headings and aliases ?
+
   test("should return unique links", () => {
     const source = "[[Page 1]] [[Page 2]] [[Page 3]] [[Page 1]]";
     const expectedLinks = ["page-1", "page-2", "page-3"];
@@ -37,16 +53,6 @@ describe("extractLinks", () => {
     expect(links).toHaveLength(0);
   });
 
-  test("should convert relative links to absolute links when file slug (not index page) is passed", () => {
-    const fileSlug = "/__blog__/abc/page-1";
-    const source = "[[../xyz/Page 2]]";
-    const expectedLinks = ["/__blog__/xyz/page-2"];
-    const links = extractLinks(source, fileSlug);
-    expect(links[0]).toBe(expectedLinks[0]);
-  });
-
-  // TODO test for index pages
-
   test("should return empty array if no links are found", () => {
     const source = "No links here";
     const links = extractLinks(source);
@@ -57,5 +63,31 @@ describe("extractLinks", () => {
     const source = "";
     const links = extractLinks(source);
     expect(links).toHaveLength(0);
+  });
+
+  describe("with base file path (slug) passed", () => {
+    test("should resolve relative links to absolute links", () => {
+      const baseFileSlug = "/__blog__/abc/page-1";
+      const source = "[[../xyz/Page 2]] [[./Page 3]] [[Page 4]]";
+      const expectedLinks = [
+        "/__blog__/xyz/page-2",
+        "/__blog__/abc/page-3",
+        "/__blog__/abc/page-4",
+      ];
+      const links = extractLinks(source, baseFileSlug);
+      expect(links[0]).toBe(expectedLinks[0]);
+    });
+
+    test("should return absolute links as is", () => {
+      const baseFileSlug = "/__blog__/abc/page-1";
+      const source = "[[/xyz/Page 2]]";
+      const expectedLinks = ["/xyz/page-2"];
+      const links = extractLinks(source, baseFileSlug);
+      expect(links[0]).toBe(expectedLinks[0]);
+    });
+
+    // TODO tests for wiki links with shortened Obsidian paths
+
+    // TODO tests for index pages
   });
 });
