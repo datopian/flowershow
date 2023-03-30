a94060cbfc60f071f89b79410096a78537648042
olayway <ola@yway.pl>
2023-03-30 21:15:15 +0200

    fix types

--- packages/markdowndb/src/lib/types.ts
+++ packages/markdowndb/src/lib/types.ts
@@ -1,30 +1,30 @@
 // File entity type
 // Maps to fields on the DB
-export type DatabaseFile<T = {}> = {
-    _id: string;
-    _path: string;
-    _url_path: string;
-    filetype: string;
-    metadata: any;
-    type: string;
+export type DatabaseFile<T = { [key: string]: any }> = {
+  _id: string;
+  _path: string;
+  _url_path: string;
+  filetype: string;
+  metadata: any;
+  type: string;
 } & T;
 
 //  Every mdx file will have tags and links
 //  Generic so that we can create custom
 //  types later... Not definite
 //  await MyMdDb.query({ filetype: ["md", "mdx"] })
-export type MDXFile<T = {}> = DatabaseFile<{
-    tags: string[];
-    links: string[];
+export type MDXFile<T = { [key: string]: any }> = DatabaseFile<{
+  tags: string[];
+  links: string[];
 }> &
-    T;
+  T;
 
 //  Optional params so that we can build complex dynamic queries
 //  E.g I want all files in the blogs folder with X and Y tags
 export interface DatabaseQuery {
-    folder?: string;
-    type?: string; // TODO
-    tags?: string[];
-    filetypes?: string[];
-    urlPath?: string;
+  folder?: string;
+  type?: string; // TODO
+  tags?: string[];
+  filetypes?: string[];
+  urlPath?: string;
 }
