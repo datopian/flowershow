7352f4b837009e0d4063fa2bb405bb20cea498e1
olayway <ola@yway.pl>
2023-04-03 18:32:27 +0200

    create parseFile util

---
+++ packages/markdowndb/src/utils/parseFile.ts
@@ -0,0 +1,66 @@
+import matter from "gray-matter";
+import { extractWikiLinks } from "./extractWikiLinks";
+
+export function parseFile(source: string) {
+  // Metadata
+  const { data: metadata } = matter(source);
+
+  // Links
+  const links = extractWikiLinks(source);
+
+  return {
+    metadata,
+    links,
+  };
+}
+
+// const tempSluggify = (str: string) => {
+//   return str
+//     .replace(/\s+/g, "-")
+//     .replace(/\.\w+$/, "")
+//     .toLowerCase();
+// };
+
+// const resolveLink = (link: string, sourcePath?: string) => {
+//   if (!sourcePath) {
+//     return link;
+//   }
+//   const dir = path.dirname(sourcePath);
+//   const resolved = path.resolve(dir, link);
+//   return resolved;
+// };
+
+// links = extractWikiLinks({
+//   source,
+//   // TODO pass slug instead of file path as hrefs/srcs are sluggified too
+//   // (where will we get it from?)
+//   filePath: tempSluggify(`/${filePath}`),
+//   ...extractWikiLinksConfig,
+// }).map((link) => {
+//   const linkEncodedPath = Buffer.from(
+//     JSON.stringify(link),
+//     "utf-8"
+//   ).toString();
+//   const linkId = crypto
+//     .createHash("sha1")
+//     .update(linkEncodedPath)
+//     .digest("hex");
+//   return {
+//     _id: linkId,
+//     from: fileId,
+//     to: link.to,
+//     link_type: link.linkType,
+//   };
+// });
+
+// SLUGGIFY
+// if (filename != "index") {
+//   if (pathToFileFolder) {
+//     _url_path = `${pathToFileFolder}/${filename}`;
+//   } else {
+//     //  The file is in the root folder
+//     _url_path = filename;
+//   }
+// } else {
+//   _url_path = pathToFileFolder;
+// }
