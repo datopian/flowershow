2f663ddfbee1bb0c400b607569fc29140a559914
olayway <ola@yway.pl>
2023-04-03 22:35:58 +0200

    adjst recursiveWalkDir

--- packages/markdowndb/src/utils/recursiveWalkDir.spec.ts
+++ packages/markdowndb/src/utils/recursiveWalkDir.spec.ts
@@ -1,23 +1,20 @@
 import { recursiveWalkDir } from "./recursiveWalkDir";
 
 describe("recursiveWalkDir", () => {
-  const contentFixturePath = "packages/markdowndb/__mocks__/content/index.mdx";
+  const contentFixturePath = "packages/markdowndb/__mocks__/content/blog";
   /*
    * To get all the file paths in the content fixture, run the following command in the terminal:
    * find <contentFicturePath> -type f
    * watch out for double slashes in the output
    * */
   const contentFixtureFilePaths = [
-    "packages/markdowndb/__mocks__/content/index.mdx",
-    "packages/markdowndb/__mocks__/content/blog/blog3.mdx",
-    "packages/markdowndb/__mocks__/content/blog/blog2.mdx",
-    "packages/markdowndb/__mocks__/content/blog/blog1.mdx",
-    "packages/markdowndb/__mocks__/content/blog0.mdx",
-    "packages/markdowndb/__mocks__/content/assets/datopian-logo.png",
+    `${contentFixturePath}/blog3.mdx`,
+    `${contentFixturePath}/blog2.mdx`,
+    `${contentFixturePath}/blog1.mdx`,
   ];
 
   test("should return all files in a directory", async () => {
-    const paths = recursiveWalkDir("packages/markdowndb/__mocks__/content");
+    const paths = recursiveWalkDir(contentFixturePath);
 
     expect(paths.length).toBe(contentFixtureFilePaths.length);
     paths.forEach((file) => {
