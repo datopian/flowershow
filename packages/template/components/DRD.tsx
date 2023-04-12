31771832ec48a7998a2cfdd2b8b8470d7c61989d
olayway <ola@yway.pl>
2023-04-12 13:44:45 +0200

    add DRD and slug from datahub-next

---
+++ packages/template/components/DRD.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { MDXRemote } from "next-mdx-remote";
+import { Mermaid } from "@flowershow/core";
+
+import layouts from "../layouts";
+
+// Custom components/renderers to pass to MDX.
+// Since the MDX files aren't loaded by webpack, they have no knowledge of how
+// to handle import statements. Instead, you must include components in scope
+// here.
+const components = {
+  mermaid: Mermaid,
+};
+
+export default function DRD({ source, frontMatter }) {
+  const Layout = ({ children }) => {
+    if (frontMatter.layout) {
+      const LayoutComponent = layouts[frontMatter.layout];
+      return <LayoutComponent {...frontMatter}>{children}</LayoutComponent>;
+    }
+    return <>{children}</>;
+  };
+
+  return (
+    <div className="prose mx-auto">
+      <header>
+        <div className="mb-6">
+          {/* Default layout */}
+          {!frontMatter.layout && (
+            <>
+              <h1>{frontMatter.title}</h1>
+              {frontMatter.author && (
+                <div className="-mt-6">
+                  <p className="opacity-60 pl-1">{frontMatter.author}</p>
+                </div>
+              )}
+              {frontMatter.description && (
+                <p className="description">{frontMatter.description}</p>
+              )}
+            </>
+          )}
+        </div>
+      </header>
+      <main>
+        <Layout>
+          <MDXRemote {...source} components={components} />
+        </Layout>
+      </main>
+    </div>
+  );
+}
--- packages/template/pages/[[...slug]].tsx
+++ packages/template/pages/[[...slug]].tsx
@@ -1,77 +1,68 @@
-/* eslint import/no-default-export: off */
-import React from "react";
 import fs from "fs";
-import { NextSeo } from "next-seo";
+import React from "react";
 
-import { getPageData } from "../lib/getPageData";
-import { getAuthorsDetails } from "../lib/getAuthorsDetails";
+import DRD from "../components/DRD";
 import clientPromise from "../lib/mddb.mjs";
-import { siteConfig } from "../config/siteConfig";
+import parse from "../lib/markdown";
+import { getAuthorsDetails } from "../lib/getAuthorsDetails";
 
-export default function Page({ source, metadata }) {
-  /* const MDXPage = useMDXComponent(source, globals); */
-  const { image, title, description } = metadata;
+export default function DRDPage({ source, frontMatter }) {
+  source = JSON.parse(source);
+  frontMatter = JSON.parse(frontMatter);
 
-  // Handle SEO Image urls in frontmatter
-  // TODO why do we remove the "/" at the end? Should images be in form of "/some_image.png"?
-  const websiteUrl = siteConfig.authorUrl.replace(/\/+$/, "");
-  const seoImageUrl =
-    image && image.startsWith("http") ? image : websiteUrl + image;
-
-  return (
-    <>
-      <NextSeo
-        title={title}
-        openGraph={{
-          title: title,
-          description: description,
-          images: image
-            ? [
-                {
-                  url: seoImageUrl,
-                  width: 1200,
-                  height: 627,
-                  alt: title,
-                },
-              ]
-            : siteConfig?.nextSeo?.openGraph?.images || [],
-        }}
-      />
-      <div>{source}</div>
-    </>
-  );
+  return <DRD source={source} frontMatter={frontMatter} />;
 }
 
-export async function getStaticProps({ params }) {
-  const mddb = await clientPromise;
-  // params.slug is undefined for root index page
+export const getStaticProps = async ({ params }) => {
   const urlPath = params.slug ? params.slug.join("/") : "/";
-  const page = await mddb.getFileByUrl(urlPath);
 
-  // Read source
-  // TODO add method on MddbFile returned from mddb
-  const source = fs.readFileSync(page!.file_path, { encoding: "utf-8" });
-  const metadata = page!.metadata || {};
+  const mddb = await clientPromise;
+  const dbFile = await mddb.getFileByUrl(urlPath);
 
-  // TODO this is crap, refactor
-  const { data, authors } = metadata || {};
-  const globals = data ? await getPageData(data) : {};
-  const authorsDetails = await getAuthorsDetails(authors);
+  const dbBacklinks = await mddb.getLinks({
+    fileId: dbFile!._id,
+    direction: "backward",
+  });
+  // TODO temporary solution, we will have a method on MddbFile to get these links
+  const dbBacklinkFilesPromises = dbBacklinks.map((link) =>
+    mddb.getFileById(link.from)
+  );
+  const dbBacklinkFiles = await Promise.all(dbBacklinkFilesPromises);
+  const dbBacklinkUrls = dbBacklinkFiles.map(
+    (file) => file!.toObject().url_path
+  );
 
-  return { props: { source, metadata, authorsDetails, globals } };
-}
+  // TODO we can already get frontmatter from dbFile.metadata
+  // so parse could only return mdxSource
+  const source = fs.readFileSync(dbFile!.file_path, { encoding: "utf-8" });
+  const { mdxSource, frontMatter } = await parse(source, "mdx", {
+    backlinks: dbBacklinkUrls,
+  });
+
+  // Temporary, so that blogs work properly
+  if (dbFile!.url_path!.startsWith("blog/")) {
+    frontMatter.layout = "blog";
+    frontMatter.authorsDetails = await getAuthorsDetails(
+      dbFile!.metadata!.authors
+    );
+  }
+
+  return {
+    props: {
+      source: JSON.stringify(mdxSource),
+      frontMatter: JSON.stringify(frontMatter),
+    },
+  };
+};
 
 export async function getStaticPaths() {
   const mddb = await clientPromise;
-  const allDocuments = await mddb.getFiles({ extensions: ["mdx", "md"] });
+  const allDocuments = await mddb.getFiles({ extensions: ["md", "mdx"] });
 
-  const paths = allDocuments
-    .filter((page) => !page.metadata?.isDraft)
-    .map((page) => {
-      // TODO files returned from db with extensions mdx and md should always have url_path
-      const parts = page.url_path!.split("/");
-      return { params: { slug: parts } };
-    });
+  const paths = allDocuments.map((page) => {
+    const parts = page!.url_path!.split("/");
+    return { params: { slug: parts } };
+  });
 
   return {
     paths,
