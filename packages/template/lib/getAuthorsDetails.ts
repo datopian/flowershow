43efb65d1a1070d05b6611f45d47764f5fa6f494
olayway <ola@yway.pl>
2023-04-12 12:36:43 +0200

    adjust blogs getter and getAuthorsDetails

--- packages/template/lib/getAuthorsDetails.ts
+++ packages/template/lib/getAuthorsDetails.ts
@@ -1,7 +1,9 @@
 import { siteConfig } from "@/config/siteConfig";
-import { allPeople } from "contentlayer/generated";
+import clientPromise from "./mddb.mjs";
 
-export const getAuthorsDetails = (authors) => {
+export const getAuthorsDetails = async (authors?: string[]) => {
+  const mddb = await clientPromise;
+  const allPeople = await mddb.getFiles({ folder: "people" });
   let blogAuthors = [];
 
   if (authors) {
@@ -12,10 +14,16 @@ export const getAuthorsDetails = (authors) => {
 
   return blogAuthors.map((author) => {
     return (
-      allPeople.find(
-        ({ id, slug, name }) =>
-          id === author || slug === author || name === author
-      ) ?? { name: author, avatar: siteConfig.avatarPlaceholder }
+      // allPeople.find(
+      //   ({ id, slug, name }) =>
+      //     id === author || slug === author || name === author
+      // ) ?? { name: author, avatar: siteConfig.avatarPlaceholder }
+
+      // TODO add back support for lookup by id and slug
+      allPeople.find((p) => p.metadata?.name === author) ?? {
+        name: author,
+        avatar: siteConfig.avatarPlaceholder,
+      }
     );
   });
 };
--- site/content/getters/blogs.js
+++ site/content/getters/blogs.js
@@ -1,12 +1,15 @@
-import { allBlogs } from "contentlayer/generated";
+import clientPromise from '@/lib/mddb';
 
-export default function getBlogs() {
-  return allBlogs
-    .map(({ title, description = null, date, url_path }) => ({
-      title,
-      description,
-      date,
-      url_path,
-    }))
-    .sort((a, b) => new Date(b.date) - new Date(a.date));
+export default async function getBlogs() {
+  const mddb = await clientPromise;
+  const allBlogs = await mddb.getFiles({ folder: 'blog' })
+  return allBlogs.map((b) => b.metadata)
+  // return allBlogs
+  //   .map(({ title, description = null, date, url_path }) => ({
+  //     title,
+  //     description,
+  //     date,
+  //     url_path,
+  //   }))
+  //   .sort((a, b) => new Date(b.date) - new Date(a.date));
 }
