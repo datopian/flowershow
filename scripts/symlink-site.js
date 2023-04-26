27839c75bab9afbacba4681911f5d6552d077663
olayway <ola@yway.pl>
2023-04-26 19:24:56 +0200

    [scripts/symlink-site][xs]: rm symlinking custom components
    - importing custom components in MD is no longer supported in Flowershow
    v.2 so this is not needed

--- scripts/symlink-site.js
+++ scripts/symlink-site.js
@@ -8,7 +8,6 @@ process.chdir(__dirname);
 const contentSymlink = "../packages/template/content";
 const assetsSymlink = "../packages/template/public/assets";
 const excalidrawSymlink = "../packages/template/public/excalidraw";
-const componentsSymlink = "../packages/template/components/custom";
 
 if (!fs.existsSync(contentSymlink)) {
   fs.symlinkSync("../../site/content", contentSymlink, "junction");
@@ -24,8 +23,4 @@ if (!fs.existsSync(excalidrawSymlink)) {
     excalidrawSymlink,
     "junction"
   );
-}
-
-if (!fs.existsSync(componentsSymlink)) {
-  fs.symlinkSync("../../../site/components", componentsSymlink, "junction");
 }
