81856e74b6a912c2166a2720463bf484a4ff5f25
olayway <ola@yway.pl>
2023-05-12 18:34:25 +0200

    [packages][m]: rm packages/template

--- packages/template/scripts/postbuild.mjs
+++
@@ -1,8 +0,0 @@
-import sitemap from "./sitemap.mjs";
-import search from "./search.mjs";
-import mddb from "./mddb.mjs";
-
-await mddb();
-await Promise.all([sitemap(), search()]);
-
-process.exit();
