bbe6a556153b9c0cbb7d09bee7f0b6dc196046ce
olayway <ola@yway.pl>
2023-06-07 12:29:06 +0200

    [examples][l]: rm all examples

--- examples/fonts-and-colors/.flowershow/lib/formatDate.js
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateUTC) {
-  return new Date(dateUTC).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    timeZone: "UTC",
-  });
-}
--- examples/wordpress-to-flowershow/content/blog.md
+++
@@ -1,6 +0,0 @@
----
-title: "Blog"
-created: 2022-12-09
-authors:
-  - rufus6922c5bca9
----
