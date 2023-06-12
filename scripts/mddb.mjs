e9c00e7985666a7cb8f585ad153dd3831548c819
olayway <ola@yway.pl>
2023-06-12 19:31:19 +0200

    [/][s]: replace @flowershow/markdowndb with mddb

--- scripts/mddb.mjs
+++ scripts/mddb.mjs
@@ -1,9 +1,9 @@
-// TODO temporary (?) file, cuz mddb command of @flowershow/markdowndb wouldn't work on Netlify
+// TODO temporary (?) file, cuz mddb command of mddb wouldn't work on Netlify
 
 import clientPromise from "../lib/mddb.mjs";
 
 export default async function mddb() {
-  const ignorePatterns = [/Excalidraw/, /.obsidian/, /DS_Store/];
+  const ignorePatterns = [/Excalidraw/, /\.obsidian/, /DS_Store/];
 
   const mddb = await clientPromise;
 
