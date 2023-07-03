afbcb0e00ee0cb2cb1bd763bb0383746117f349a
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-07-03 16:57:53 +0200

    [package.json,scripts/sitemap][xs]: generate sitemap from R2 objects

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -1,17 +1,32 @@
 import { writeFileSync } from "fs";
 import { globby } from "globby";
 import prettier from "prettier";
+import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
 
 import config from "../content/config.mjs";
-import clientPromise from "../lib/mddb.mjs";
 
 export default async function sitemap() {
   const prettierConfig = await prettier.resolveConfig("");
-  const mddb = await clientPromise;
-  const allFiles = await mddb.getFiles({ extensions: ["mdx", "md"] });
-  const contentPages = allFiles
-    .filter((x) => !x.metadata?.isDraft)
-    .map((x) => `/${x.url_path}`);
+
+  const S3 = new S3Client({
+    region: "auto",
+    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+      accessKeyId: process.env.R2_ACCESS_KEY_ID,
+      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    },
+  });
+
+  // TODO this will only fetch up to 1000 objects !!
+  const allR2Objects = await S3.send(
+    new ListObjectsV2Command({
+      Bucket: process.env.R2_BUCKET_NAME
+    })
+  )
+
+  const contentPages = allR2Objects
+    .Contents
+    .map((x) => `/${x.Key.replace(/\.mdx?$/, "")}`)
   const pages = await globby([
     "pages/*.(js|tsx)",
     "!pages/_*.(js|tsx)",
@@ -22,7 +37,8 @@ export default async function sitemap() {
 
   const siteUrl = config?.domain?.replace(/\/$/, "");
 
-  const sitemap = `
+  if (siteUrl) {
+    const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${pages
@@ -44,12 +60,10 @@ export default async function sitemap() {
     </urlset>
   `;
 
-  const formatted = prettier.format(sitemap, {
-    ...prettierConfig,
-    parser: "html",
-  });
-
-  if (siteUrl) {
+    const formatted = prettier.format(sitemap, {
+      ...prettierConfig,
+      parser: "html",
+    });
     writeFileSync("public/sitemap.xml", formatted);
     console.log("Sitemap generated...");
   }
