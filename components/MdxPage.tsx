1d882d5dc00caa4e3c945f4f66dccc4e4ab2cd06
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-09-19 16:59:59 +0200

    [MdxPage.tsx,lib/markdown][s]: fix KaTeX rendering

--- components/MdxPage.tsx
+++ components/MdxPage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Head from "next/head";
+
 import { MDXRemote } from "next-mdx-remote";
 import { Mermaid, Pre } from "@portaljs/core";
 
@@ -9,24 +11,28 @@ import layouts from "../layouts";
 // to handle import statements. Instead, you must include components in scope
 // here.
 const components = {
-  mermaid: Mermaid,
-  pre: Pre,
+    mermaid: Mermaid,
+    pre: Pre,
 };
 
 export default function MdxPage({ source, frontMatter }) {
-  const Layout = ({ children }) => {
-    if (frontMatter.layout) {
-      const LayoutComponent = layouts[frontMatter.layout];
-      return <LayoutComponent {...frontMatter}>{children}</LayoutComponent>;
-    }
-    return <>{children}</>;
-  };
+    const Layout = ({ children }) => {
+        if (frontMatter.layout) {
+            const LayoutComponent = layouts[frontMatter.layout];
+            return <LayoutComponent {...frontMatter}>{children}</LayoutComponent>;
+        }
+        return <>{children}</>;
+    };
 
-  return (
-    <main id="mdxpage" className="prose mx-auto">
-      <Layout>
-        <MDXRemote {...source} components={components} />
-      </Layout>
-    </main>
-  );
+    return (
+        <main id="mdxpage" className="prose mx-auto">
+            <Head>
+                {/* KaTeX styles */}
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css" integrity="sha384-GvrOXuhMATgEsSwCs4smul74iXGOixntILdUW9XmUC6+HX0sLNAK3q71HotJqlAn" crossOrigin="anonymous" />
+            </Head>
+            <Layout>
+                <MDXRemote {...source} components={components} />
+            </Layout>
+        </main>
+    );
 }
