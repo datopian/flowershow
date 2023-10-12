499e77d627c7a7597f1cf3801f59db8b666ca1af
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-10-12 18:54:07 +0200

    [next.config][xs]: add rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@ export default {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/@:org/:slug*",
+          destination: "/@org/:org/:slug*",
+        },
+      ],
+    };
+  },
   webpack: (config) => {
     config.infrastructureLogging = {
       level: "error",
