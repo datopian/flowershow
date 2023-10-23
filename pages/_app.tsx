5723ef5430f15ddc99b57c1bffcb2ee8e6717bf7
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-10-23 14:11:39 +0200

    custom app sugesstions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import Script from "next/script";
-import { DefaultSeo } from "next-seo";
+import { DefaultSeo } from "next-seo";  // SUGGESTION: if we upgrade to App Router, we won't need
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import {
-    Layout,
+    Layout, // SUGGESTION: eject it; it's usually sth we (and tech-savvy users) want to customize; only it's specific components should be available in the core package
     SearchProvider,
-    pageview,
-    ThemeProvider,
+    pageview, // SUGGESTION: this should be a part of a separate component: GoogleAnalytics (along with the two scripts and the useEffect below)
+    ThemeProvider, // SUGGESTION: eject it; this is just re-exported from the core package for no reason
 } from "@portaljs/core";
 import type { NavGroup, NavItem } from "@portaljs/core";
 
@@ -36,13 +36,20 @@ const MyApp = ({ Component, pageProps }: AppProps<CustomAppProps>) => {
     const router = useRouter();
     const { meta, siteMap } = pageProps;
 
+    // SUGGESTION: when we upgrade to App Router, the Layout component (once we eject it)
+    // will be able to fetch any data it needs on it's own
+    // ALSO (no matter if we upgrade or not) split the layout into two components:
+    // 1) top level layout that imports site-wide, non-dynamic settings from siteConfig and is used here (or as a root Layout in App router)
+    // 2) a LayoutRenderer component that get's the per-page, dynamic settings from the pageProps (or again, fetches them on it's own if we upgrade to App Router)
     const layoutProps = {
+        // SUGGESTION: these would go to the root Layout component
         showToc: meta?.showToc,
         showEditLink: meta?.showEditLink,
         showSidebar: meta?.showSidebar,
         showComments: meta?.showComments,
         editUrl: meta?.editUrl,
         urlPath: meta?.urlPath,
+        // SUGGESTION: these would be passed to (or fetched in) the LayoutRenderer component
         commentsConfig: siteConfig.comments,
         nav: {
             title: siteConfig.navbarTitle?.text ?? siteConfig.title,
@@ -60,9 +67,10 @@ const MyApp = ({ Component, pageProps }: AppProps<CustomAppProps>) => {
             defaultTheme: siteConfig.theme.default,
             themeToggleIcon: siteConfig.theme.toggleIcon,
         },
-        siteMap,
+        siteMap, // SUGGESTION: again, this could be generated in the root Layout if we upgrade to App Router
     };
 
+    // SUGGESTION: move this to a separate component: GoogleAnalytics
     useEffect(() => {
         if (siteConfig.analytics) {
             const handleRouteChange = (url) => {
@@ -85,6 +93,7 @@ const MyApp = ({ Component, pageProps }: AppProps<CustomAppProps>) => {
             <DefaultSeo defaultTitle={siteConfig.title} {...siteConfig.nextSeo} />
             {/* Global Site Tag (gtag.js) - Google Analytics */}
             {siteConfig.analytics && (
+                // SUGGESTION: move these two Google Analytics scripts to a separate component: GoogleAnalytics
                 <>
                     <Script
                         strategy="afterInteractive"
